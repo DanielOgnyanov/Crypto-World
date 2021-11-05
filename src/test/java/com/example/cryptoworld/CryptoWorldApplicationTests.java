c8843327ca48ef519b9e8eb0f5b81292683d9d02
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-05 21:35:01 +0200

    Initial settings

---
+++ src/test/java/com/example/cryptoworld/CryptoWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptoworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
