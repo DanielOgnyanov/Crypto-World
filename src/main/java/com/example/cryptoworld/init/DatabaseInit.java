74f3066881c84297af475931deeba00d0c1ab2f4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 22:10:00 +0200

    db init country

--- src/main/java/com/example/cryptoworld/init/DatabaseInit.java
+++ src/main/java/com/example/cryptoworld/init/DatabaseInit.java
@@ -1,11 +1,17 @@
 package com.example.cryptoworld.init;
 
+import com.example.cryptoworld.service.CountryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseInit implements CommandLineRunner {
 
+    private final CountryService countryService;
+
+    public DatabaseInit(CountryService countryService) {
+        this.countryService = countryService;
+    }
 
     @Override
     public void run(String... args) throws Exception {
--- src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
@@ -1,4 +1,8 @@
 package com.example.cryptoworld.service.Impl;
 
-public class CountryServiceImpl {
+import com.example.cryptoworld.service.CountryService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryServiceImpl implements CountryService {
 }
