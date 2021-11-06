c8c46c20295594f1b301d1e3d08fbbc2deb6fc2b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 22:11:53 +0200

    db init country

--- src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
@@ -1,14 +1,17 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.CountryRepository;
 import com.example.cryptoworld.service.CountryService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CountryServiceImpl implements CountryService {
 
+    private final CountryRepository countryRepository;
 
-
-
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        this.countryRepository = countryRepository;
+    }
 
 
     @Override
