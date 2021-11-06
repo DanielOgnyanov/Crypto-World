235694952b970c2df0ac711b31be85780a1ac6e0
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 22:13:50 +0200

    db init country

--- src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CountryServiceImpl.java
@@ -1,8 +1,12 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.models.entities.CountryEntity;
+import com.example.cryptoworld.models.enums.EnumCountry;
 import com.example.cryptoworld.repository.CountryRepository;
 import com.example.cryptoworld.service.CountryService;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class CountryServiceImpl implements CountryService {
@@ -17,5 +21,17 @@ public class CountryServiceImpl implements CountryService {
     @Override
     public void InitCountryInDb() {
 
+        if (countryRepository.count() == 0) {
+
+            Arrays.stream(EnumCountry.values())
+                    .forEach(countryName -> {
+
+                        CountryEntity countryEntity = new CountryEntity(countryName);
+
+                        countryRepository.save(countryEntity);
+
+
+                    });
+        }
     }
 }
