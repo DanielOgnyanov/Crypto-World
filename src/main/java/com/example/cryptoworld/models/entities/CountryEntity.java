59f964d2d6717dd23ffc0f9c8c5f44df377f9322
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 21:44:49 +0200

    enum country

--- src/main/java/com/example/cryptoworld/models/entities/CountryEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/CountryEntity.java
@@ -1,9 +1,18 @@
 package com.example.cryptoworld.models.entities;
 
+import com.example.cryptoworld.models.enums.EnumCountry;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "countries")
 public class CountryEntity extends BaseEntity{
+
+    private EnumCountry name;
+
+
+
+
+
 }
