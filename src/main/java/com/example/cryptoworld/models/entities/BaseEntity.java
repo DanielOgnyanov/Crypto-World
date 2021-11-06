262936426bb2e8993f245fcaef50387c5add96c6
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 20:57:29 +0200

    Base entity

--- src/main/java/com/example/cryptoworld/models/entities/BaseEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/BaseEntity.java
@@ -1,7 +1,23 @@
 package com.example.cryptoworld.models.entities;
 
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 
 @MappedSuperclass
 public abstract class BaseEntity {
+
+    private Long id;
+
+    public BaseEntity() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false,unique = true)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
