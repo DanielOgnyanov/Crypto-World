8ef569e271a65ca0a37f526204d3ef3bc581368b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-06 21:08:39 +0200

    User entity

--- src/main/java/com/example/cryptoworld/models/entities/UserEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/UserEntity.java
@@ -1,6 +1,7 @@
 package com.example.cryptoworld.models.entities;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -8,12 +9,57 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class UserEntity extends BaseEntity{
 
+    @Column(name = "username", unique = true)
     private String username;
+    @Column(name = "full_name")
     private String fullName;
+    @Column(name = "password")
     private String password;
+    @Column(name = "email", unique = true)
     private String email;
+    @Column(name = "country")
     private String country;
 
     public UserEntity() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 }
