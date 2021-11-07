9a121ae4a0bdc04279f668b9dc22d59e3ace3f24
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-07 17:07:17 +0200

    credit card entity

---
+++ src/main/java/com/example/cryptoworld/models/entities/CreditCardEntity.java
@@ -0,0 +1,10 @@
+package com.example.cryptoworld.models.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "credit_cards")
+public class CreditCardEntity extends BaseEntity{
+}
