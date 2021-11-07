eb65102afe1cf13a5220046ec59a19fe670f84e9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-07 16:35:14 +0200

    role service

--- src/main/java/com/example/cryptoworld/service/Impl/RoleServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/RoleServiceImpl.java
@@ -1,10 +1,19 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.RoleRepository;
 import com.example.cryptoworld.service.RoleService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+
     @Override
     public void InitRoleInDb() {
 
