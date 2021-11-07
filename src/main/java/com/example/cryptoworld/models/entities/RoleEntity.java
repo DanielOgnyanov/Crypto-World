efa6698343eb8d37ab6a0623a574ecbc3bd7de37
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-07 16:26:02 +0200

    role entity

---
+++ src/main/java/com/example/cryptoworld/models/entities/RoleEntity.java
@@ -0,0 +1,10 @@
+package com.example.cryptoworld.models.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class RoleEntity extends BaseEntity{
+}
