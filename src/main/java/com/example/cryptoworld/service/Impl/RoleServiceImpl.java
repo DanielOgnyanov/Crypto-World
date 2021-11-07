143c462f3a3202874fd0a6a28ddb9ee606c6c8a9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-07 16:33:03 +0200

    role service

---
+++ src/main/java/com/example/cryptoworld/service/Impl/RoleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cryptoworld.service.Impl;
+
+import com.example.cryptoworld.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+}
