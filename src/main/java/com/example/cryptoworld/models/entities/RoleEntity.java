9f2b183f3b524f5510504deabe9fbf577825c93a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-07 16:27:17 +0200

    role entity

--- src/main/java/com/example/cryptoworld/models/entities/RoleEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/RoleEntity.java
@@ -1,10 +1,23 @@
 package com.example.cryptoworld.models.entities;
 
 
+import com.example.cryptoworld.models.enums.EnumRole;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class RoleEntity extends BaseEntity{
+
+    private EnumRole role;
+
+
+    public EnumRole getRole() {
+        return role;
+    }
+
+    public void setRole(EnumRole role) {
+        this.role = role;
+    }
 }
