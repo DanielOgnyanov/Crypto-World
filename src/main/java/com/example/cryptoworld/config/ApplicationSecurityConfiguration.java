5e4c8975f43684e53c9719a52066559ac407c2d3
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-08 18:38:22 +0200

    config spring security

--- src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.cryptoworld.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,4 +21,14 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     }
 
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+
+                .antMatchers("/", "/users/login", "/users/register").permitAll()
+
+
+
+    }
 }
