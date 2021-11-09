8fdf36a1606a88e912b5e7e55ebf626b31611edd
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:50:19 +0200

    crypto service

---
+++ src/main/java/com/example/cryptoworld/service/Impl/CryptoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cryptoworld.service.Impl;
+
+import com.example.cryptoworld.service.CryptoSrvice;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CryptoServiceImpl implements CryptoSrvice {
+}
