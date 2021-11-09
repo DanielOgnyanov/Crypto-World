5dc692219c07b84dd242d816f3ca5203328601a5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:53:13 +0200

    init crypto in db

--- src/main/java/com/example/cryptoworld/init/DatabaseInit.java
+++ src/main/java/com/example/cryptoworld/init/DatabaseInit.java
@@ -1,6 +1,7 @@
 package com.example.cryptoworld.init;
 
 import com.example.cryptoworld.service.CountryService;
+import com.example.cryptoworld.service.CryptoSrvice;
 import com.example.cryptoworld.service.RoleService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -10,10 +11,12 @@ public class DatabaseInit implements CommandLineRunner {
 
     private final CountryService countryService;
     private final RoleService roleService;
+    private final CryptoSrvice cryptoSrvice;
 
-    public DatabaseInit(CountryService countryService, RoleService roleService) {
+    public DatabaseInit(CountryService countryService, RoleService roleService, CryptoSrvice cryptoSrvice) {
         this.countryService = countryService;
         this.roleService = roleService;
+        this.cryptoSrvice = cryptoSrvice;
     }
 
     @Override
