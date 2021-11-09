a4fb79378e54aed1c3b6b8700ff745440d242eca
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:38:31 +0200

    crypto enum

--- src/main/java/com/example/cryptoworld/models/enums/EnumCryptoTop10.java
+++ src/main/java/com/example/cryptoworld/models/enums/EnumCryptoTop10.java
@@ -1,4 +1,28 @@
 package com.example.cryptoworld.models.enums;
 
-public class EnumCryptoTop10 {
+import java.math.BigDecimal;
+
+public enum EnumCryptoTop10 {
+
+    Bitcoin(66000),
+    Ethereum(4700),
+    Binance(642),
+    Cardano(2.28),
+    Tether(1),
+    Solana(240),
+    XRP(1.26),
+    Polkadot(51),
+    Dogecoin(0.27),
+    usdcoin(1);
+
+
+    private double price;
+
+    EnumCryptoTop10(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }
