5da69e48439fbeb6658f2c1e851ed9f85345a32c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:41:23 +0200

    crypto entity

--- src/main/java/com/example/cryptoworld/models/entities/CryptoCurrenciesEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/CryptoCurrenciesEntity.java
@@ -1,10 +1,18 @@
 package com.example.cryptoworld.models.entities;
 
 
+import com.example.cryptoworld.models.enums.EnumCryptoTop10;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "crypto_currencies")
 public class CryptoCurrenciesEntity extends BaseEntity{
+
+    private EnumCryptoTop10 currencies;
+    private double price;
+
+
+
 }
