e7b071d5fb67217d0eeccbbea427d2a29b249d32
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 19:12:59 +0200

    log deposit entity

--- src/main/java/com/example/cryptoworld/models/entities/LogDeposit.java
+++ src/main/java/com/example/cryptoworld/models/entities/LogDeposit.java
@@ -3,8 +3,41 @@ package com.example.cryptoworld.models.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "log_deposit")
 public class LogDeposit extends BaseEntity{
+
+    private UserEntity depositPerson;
+    private BigDecimal fiatMoney;
+    private BigDecimal cryptoValue;
+
+
+    public LogDeposit() {
+    }
+
+    public UserEntity getDepositPerson() {
+        return depositPerson;
+    }
+
+    public void setDepositPerson(UserEntity depositPerson) {
+        this.depositPerson = depositPerson;
+    }
+
+    public BigDecimal getFiatMoney() {
+        return fiatMoney;
+    }
+
+    public void setFiatMoney(BigDecimal fiatMoney) {
+        this.fiatMoney = fiatMoney;
+    }
+
+    public BigDecimal getCryptoValue() {
+        return cryptoValue;
+    }
+
+    public void setCryptoValue(BigDecimal cryptoValue) {
+        this.cryptoValue = cryptoValue;
+    }
 }
