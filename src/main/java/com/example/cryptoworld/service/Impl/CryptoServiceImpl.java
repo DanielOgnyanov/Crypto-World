fac6879de53b927e860a3ac3c92cdbcde1822c45
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:57:25 +0200

    init crypto in db

--- src/main/java/com/example/cryptoworld/service/Impl/CryptoServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CryptoServiceImpl.java
@@ -1,8 +1,13 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.models.entities.CryptoCurrenciesEntity;
+import com.example.cryptoworld.models.enums.EnumCountry;
+import com.example.cryptoworld.models.enums.EnumCryptoTop10;
 import com.example.cryptoworld.repository.CryptoRepository;
 import com.example.cryptoworld.service.CryptoService;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class CryptoServiceImpl implements CryptoService {
@@ -18,5 +23,20 @@ public class CryptoServiceImpl implements CryptoService {
     @Override
     public void InitCryptoInDb() {
 
+        if (cryptoRepository.count() == 0) {
+
+            Arrays.stream(EnumCryptoTop10.values())
+                    .forEach(currentValue -> {
+
+                        CryptoCurrenciesEntity currCrypto =
+                                new CryptoCurrenciesEntity(currentValue, currentValue.getPrice());
+
+                        cryptoRepository.save(currCrypto);
+
+
+                    });
+
+
+        }
     }
 }
