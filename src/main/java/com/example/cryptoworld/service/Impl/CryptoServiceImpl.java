299d9c892405442e6cd9dcbc1abe1b1e6cc3f929
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:55:13 +0200

    init crypto in db

--- src/main/java/com/example/cryptoworld/service/Impl/CryptoServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CryptoServiceImpl.java
@@ -1,10 +1,20 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.CryptoRepository;
 import com.example.cryptoworld.service.CryptoService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CryptoServiceImpl implements CryptoService {
+
+
+    private final CryptoRepository cryptoRepository;
+
+    public CryptoServiceImpl(CryptoRepository cryptoRepository) {
+        this.cryptoRepository = cryptoRepository;
+    }
+
+
     @Override
     public void InitCryptoInDb() {
 
