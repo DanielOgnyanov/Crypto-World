d230d55509f55d500db4120ac0b70cbf4f2e17b4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-09 18:28:41 +0200

    crypto entity

---
+++ src/main/java/com/example/cryptoworld/models/entities/CryptoCurrenciesEntity.java
@@ -0,0 +1,10 @@
+package com.example.cryptoworld.models.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "crypto_currencies")
+public class CryptoCurrenciesEntity {
+}
