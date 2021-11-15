1cd7dc2f9728ccc652a45e9fb557554fc477f0c1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 20:49:08 +0200

    user register binding model

--- src/main/java/com/example/cryptoworld/models/binding/UserRegistrationBindingModel.java
+++ src/main/java/com/example/cryptoworld/models/binding/UserRegistrationBindingModel.java
@@ -1,4 +1,11 @@
 package com.example.cryptoworld.models.binding;
 
 public class UserRegistrationBindingModel {
+
+
+    private String username;
+    private String fullName;
+    private String email;
+    private String password;
+    private String confirmPassword;
 }
