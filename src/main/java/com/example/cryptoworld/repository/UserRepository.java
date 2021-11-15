64af46d1a58da7b6800ce9b72743fff1167a8575
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:25:39 +0200

    find existing username

--- src/main/java/com/example/cryptoworld/repository/UserRepository.java
+++ src/main/java/com/example/cryptoworld/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.example.cryptoworld.repository;
 
 import com.example.cryptoworld.models.entities.UserEntity;
+import com.fasterxml.jackson.databind.introspect.AnnotationCollector;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
 }
