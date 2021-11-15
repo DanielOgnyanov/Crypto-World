cad2e3d0ce3356fcd16e3945a23c97cc7e7b14dc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:24:52 +0200

    find existing username

--- src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -20,6 +20,6 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public boolean userNameExists(String username) {
-        return false;
+        return userRepository.findByUsername(username).isPresent();
     }
 }
