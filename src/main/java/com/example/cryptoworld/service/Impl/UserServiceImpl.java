b73df6f65acc6055c231f019a39215ad2cc586f1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:24:15 +0200

    find existing username

--- src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -1,17 +1,21 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.UserRepository;
 import com.example.cryptoworld.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
 
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
 
-
-
-
-
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+    }
 
 
     @Override
