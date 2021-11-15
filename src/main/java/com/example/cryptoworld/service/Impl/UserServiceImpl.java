29bd2149e492495d176c96586704d8117f3ff9a9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:55:19 +0200

    registerAndLoginUser method

--- src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -20,10 +20,11 @@ public class UserServiceImpl implements UserService {
     private final PasswordEncoder passwordEncoder;
     private final RoleRepository roleRepository;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
         this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
 
