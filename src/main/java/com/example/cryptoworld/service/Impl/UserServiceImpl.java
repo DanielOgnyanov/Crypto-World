ae4b400814cc86d0a193f689bba2ca08f4d2c26a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:08:15 +0200

    user service

---
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cryptoworld.service.Impl;
+
+import com.example.cryptoworld.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
