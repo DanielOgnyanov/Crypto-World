4be49c6815d8f6cdfb6e873b4e92e219898a85ec
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:07:12 +0200

    user controller setting

--- src/main/java/com/example/cryptoworld/web/UserController.java
+++ src/main/java/com/example/cryptoworld/web/UserController.java
@@ -1,6 +1,7 @@
 package com.example.cryptoworld.web;
 
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/users")
 public class UserController {
+
+
+    private final ModelMapper modelMapper;
+
+    public UserController(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
 
     @GetMapping("/create")
     public String create(Model model) {
