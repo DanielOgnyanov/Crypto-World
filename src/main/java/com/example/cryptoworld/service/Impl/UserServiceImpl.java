b1a90b6cee7b0c24697052bcb5cbd56ef72d4796
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 21:47:03 +0200

    registerAndLoginUser method

--- src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.example.cryptoworld.service.Impl;
 
 import com.example.cryptoworld.models.entities.RoleEntity;
 import com.example.cryptoworld.models.entities.UserEntity;
+import com.example.cryptoworld.models.enums.EnumRole;
 import com.example.cryptoworld.models.service.UserRegistrationServiceModel;
+import com.example.cryptoworld.repository.RoleRepository;
 import com.example.cryptoworld.repository.UserRepository;
 import com.example.cryptoworld.service.UserService;
 import org.modelmapper.ModelMapper;
@@ -16,6 +18,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -37,8 +40,9 @@ public class UserServiceImpl implements UserService {
         currUser.setPassword(passwordEncoder.encode(serviceModel.getPassword()));
 
 
-        RoleEntity roleUser = userRepository
-                .
+        RoleEntity roleUser = roleRepository
+                .findByRole(EnumRole.USER).orElseThrow(
+                        () -> new IllegalStateException("USER role not found. Please seed the roles."));
 
     }
 }
