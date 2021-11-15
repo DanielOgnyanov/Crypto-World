8fb57ec2bf8dbd19aa5c140ff61f0121568941ad
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-15 20:50:24 +0200

    user register binding model

--- src/main/java/com/example/cryptoworld/models/binding/UserRegistrationBindingModel.java
+++ src/main/java/com/example/cryptoworld/models/binding/UserRegistrationBindingModel.java
@@ -1,5 +1,8 @@
 package com.example.cryptoworld.models.binding;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class UserRegistrationBindingModel {
 
 
@@ -8,4 +11,50 @@ public class UserRegistrationBindingModel {
     private String email;
     private String password;
     private String confirmPassword;
+
+
+    public UserRegistrationBindingModel() {
+    }
+
+    @NotEmpty
+    @Size(min = 3)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
