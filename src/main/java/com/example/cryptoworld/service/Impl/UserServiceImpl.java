01b38899f7419428ff46b25ecd7e38ac627de2a4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-16 18:18:51 +0200

    user load by username method

--- src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import com.example.cryptoworld.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -66,6 +67,7 @@ public class UserServiceImpl implements UserService {
         );
 
 
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
     }
 }
