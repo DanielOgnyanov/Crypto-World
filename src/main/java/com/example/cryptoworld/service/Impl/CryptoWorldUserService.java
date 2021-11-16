5d5e65b9a495b15463ba1797d1dad03548f8131c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-16 18:12:44 +0200

    user load by username method

--- src/main/java/com/example/cryptoworld/service/Impl/CryptoWorldUserService.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CryptoWorldUserService.java
@@ -2,10 +2,16 @@ package com.example.cryptoworld.service.Impl;
 
 import com.example.cryptoworld.models.entities.UserEntity;
 import com.example.cryptoworld.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class CryptoWorldUserService implements UserDetailsService {
@@ -26,6 +32,21 @@ public class CryptoWorldUserService implements UserDetailsService {
                 .orElseThrow(() -> new UsernameNotFoundException("User with name " + username + " was not found!"));
 
 
-        return null;
+        return mapToUserDetails(userEntity);
+    }
+
+    private UserDetails mapToUserDetails(UserEntity userEntity) {
+        List<GrantedAuthority> authorities =
+                userEntity.
+                        getRoles().
+                        stream().
+                        map(r -> new SimpleGrantedAuthority("ROLE_" + r.getRole().name())).
+                        collect(Collectors.toList());
+
+        return new User(
+                userEntity.getUsername(),
+                userEntity.getPassword(),
+                authorities
+        );
     }
 }
