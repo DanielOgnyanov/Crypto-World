9ee60cada006cbfa89116c5d765da29341e492a5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-17 21:53:29 +0200

    setting spring security

--- src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
@@ -5,12 +5,14 @@ import com.example.cryptoworld.models.enums.EnumRole;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 
