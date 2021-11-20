c8f101148d666674fe2010357366d40cbde6b489
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-20 17:01:38 +0200

    load by username method

--- src/main/java/com/example/cryptoworld/repository/UserRepository.java
+++ src/main/java/com/example/cryptoworld/repository/UserRepository.java
@@ -11,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
+
+    Optional<UserEntity> findByUsername(String username);
 }
--- src/main/java/com/example/cryptoworld/service/Impl/CryptoWorldUserService.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CryptoWorldUserService.java
@@ -1,14 +1,49 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.models.entities.UserEntity;
+import com.example.cryptoworld.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class CryptoWorldUserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CryptoWorldUserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.
+                findByUsername(username).
+                orElseThrow(() -> new UsernameNotFoundException("User with name " + username + " was not found!"));
+
+        return mapToUserDetails(userEntity);
+    }
+
+    private UserDetails mapToUserDetails(UserEntity userEntity) {
+        List<GrantedAuthority> authorities =
+                userEntity.
+                        getRoles().
+                        stream().
+                        map(r -> new SimpleGrantedAuthority("ROLE_" + r.getRole().name())).
+                        collect(Collectors.toList());
+
+        return new User(
+                userEntity.getUsername(),
+                userEntity.getPassword(),
+                authorities
+        );
     }
 }
