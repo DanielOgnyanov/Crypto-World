ca42536a204ce6a09f092e2762fe24214667058d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-22 21:02:45 +0200

    model attribute card in controller

--- src/main/java/com/example/cryptoworld/web/CreditCardController.java
+++ src/main/java/com/example/cryptoworld/web/CreditCardController.java
@@ -1,16 +1,33 @@
 package com.example.cryptoworld.web;
 
+import com.example.cryptoworld.models.binding.UserRegistrationBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/card")
 public class CreditCardController {
 
+
+
+
+    // Model Attribute
+
+    @ModelAttribute("addCreditCardBindingModel")
+    public CreditCardController createBindingModel() {
+        return new CreditCardController();
+    }
+
+
+
     @GetMapping("/add")
     public String card() {
 
         return "card";
     }
+
+
+
 }
