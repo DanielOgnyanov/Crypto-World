fadf9e812302a50e15a13713cb6fec6c48ceffbc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-22 22:18:15 +0200

    add card method in service

--- src/main/java/com/example/cryptoworld/service/CreditCartService.java
+++ src/main/java/com/example/cryptoworld/service/CreditCartService.java
@@ -1,4 +1,7 @@
 package com.example.cryptoworld.service;
 
+import com.example.cryptoworld.models.service.AddCreditCardServiceModel;
+
 public interface CreditCartService {
+    void addCreditCard(AddCreditCardServiceModel addCreditCardServiceModel);
 }
--- src/main/java/com/example/cryptoworld/service/Impl/CreditCardServiceImpl.java
+++ src/main/java/com/example/cryptoworld/service/Impl/CreditCardServiceImpl.java
@@ -1,8 +1,47 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.models.entities.UserEntity;
+import com.example.cryptoworld.models.service.AddCreditCardServiceModel;
+import com.example.cryptoworld.repository.CreditCardRepository;
+import com.example.cryptoworld.repository.UserRepository;
 import com.example.cryptoworld.service.CreditCartService;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreditCardServiceImpl implements CreditCartService {
+
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final CreditCardRepository creditCardRepository;
+
+    public CreditCardServiceImpl(UserRepository userRepository, ModelMapper modelMapper, CreditCardRepository creditCardRepository) {
+        this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.creditCardRepository = creditCardRepository;
+    }
+
+
+    @Override
+    public void addCreditCard(AddCreditCardServiceModel addCreditCardServiceModel) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = authentication.getName();
+
+
+        UserEntity userEntity = userRepository.findByUsername(username).orElse(null);
+
+
+
+
+
+
+
+
+
+        System.out.println();
+
+    }
 }
