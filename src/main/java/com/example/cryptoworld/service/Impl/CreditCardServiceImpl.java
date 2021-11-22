a2178ef05a07c1162117237b43420e433fe3e412
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-22 20:44:41 +0200

    card service

---
+++ src/main/java/com/example/cryptoworld/service/Impl/CreditCardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.cryptoworld.service.Impl;
+
+import com.example.cryptoworld.service.CreditCartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreditCardServiceImpl implements CreditCartService {
+}
