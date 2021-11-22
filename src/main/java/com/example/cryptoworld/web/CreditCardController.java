cb7a791e1ad5822e8e12065ff6051f1de9f7353a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-22 22:27:38 +0200

    card controller post mapping

--- src/main/java/com/example/cryptoworld/web/CreditCardController.java
+++ src/main/java/com/example/cryptoworld/web/CreditCardController.java
@@ -1,12 +1,13 @@
 package com.example.cryptoworld.web;
 
 import com.example.cryptoworld.models.binding.AddCreditCardBindingModel;
-import com.example.cryptoworld.models.binding.UserRegistrationBindingModel;
+import com.example.cryptoworld.models.service.AddCreditCardServiceModel;
+import com.example.cryptoworld.service.CreditCartService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -17,7 +18,13 @@ import javax.validation.Valid;
 @RequestMapping("/card")
 public class CreditCardController {
 
+    private final ModelMapper modelMapper;
+    private final CreditCartService creditCartService;
 
+    public CreditCardController(ModelMapper modelMapper, CreditCartService creditCartService) {
+        this.modelMapper = modelMapper;
+        this.creditCartService = creditCartService;
+    }
 
     @GetMapping("/add")
     public String card(Model model) {
@@ -28,11 +35,10 @@ public class CreditCardController {
     }
 
 
-
     @PostMapping("/add")
     public String addCardConfirm(@Valid AddCreditCardBindingModel addCreditCardBindingModel,
                                  BindingResult bindingResult,
-                                 RedirectAttributes redirectAttributes){
+                                 RedirectAttributes redirectAttributes) {
 
 
         if (bindingResult.hasErrors()) {
@@ -44,17 +50,11 @@ public class CreditCardController {
         }
 
 
-
-
-
-
-
-
+        creditCartService.addCreditCard(modelMapper.map(addCreditCardBindingModel, AddCreditCardServiceModel.class));
 
 
         return "redirect:home";
     }
-
 
 
 }
