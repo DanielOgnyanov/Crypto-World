e81c3e2d5497df4cf635a4015dd2c4b3a85866d2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-23 18:22:59 +0200

    log deposit service

---
+++ src/main/java/com/example/cryptoworld/service/Impl/LogDepositServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.cryptoworld.service.Impl;
+
+import com.example.cryptoworld.models.entities.LogDeposit;
+import com.example.cryptoworld.service.LogDepositService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogDepositServiceImpl implements LogDepositService {
+}
