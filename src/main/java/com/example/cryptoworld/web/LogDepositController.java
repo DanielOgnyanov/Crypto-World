3277f82dc694c76455bd3e6b5b36d509ef4cd824
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-23 18:27:53 +0200

    log deposit controller get mapping

--- src/main/java/com/example/cryptoworld/web/LogDepositController.java
+++ src/main/java/com/example/cryptoworld/web/LogDepositController.java
@@ -1,7 +1,22 @@
 package com.example.cryptoworld.web;
 
+import com.example.cryptoworld.models.binding.AddCreditCardBindingModel;
+import com.example.cryptoworld.models.binding.LogDepositBindingModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/deposit")
 public class LogDepositController {
+
+    @GetMapping("/add")
+    public String card(Model model) {
+
+        if (!model.containsAttribute("logDepositBindingModel")) {
+            model.addAttribute("logDepositBindingModel", new LogDepositBindingModel());
+        }
+        return "log";
+    }
 }
