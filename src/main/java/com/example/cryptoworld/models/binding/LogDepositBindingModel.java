709b698b5f7f2d1c7ec47f00cf0b281d69c8a4b0
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-23 18:20:29 +0200

    log deposit binding model error messages

--- src/main/java/com/example/cryptoworld/models/binding/LogDepositBindingModel.java
+++ src/main/java/com/example/cryptoworld/models/binding/LogDepositBindingModel.java
@@ -1,18 +1,23 @@
 package com.example.cryptoworld.models.binding;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 public class LogDepositBindingModel {
 
 
     private String usernameConfirm;
-    private BigDecimal priceValue;
+    private BigDecimal deposit;
 
 
     public LogDepositBindingModel() {
     }
-
-
+    @NotNull(message = "Username cannot be empty !")
+    @NotEmpty(message = "Username cannot be empty !")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters !")
     public String getUsernameConfirm() {
         return usernameConfirm;
     }
@@ -21,11 +26,13 @@ public class LogDepositBindingModel {
         this.usernameConfirm = usernameConfirm;
     }
 
-    public BigDecimal getPriceValue() {
-        return priceValue;
+    @NotNull(message = "Deposit cannot be empty !")
+    @DecimalMin(value = "10", message = "Deposit must be Positive and over 10!")
+    public BigDecimal getDeposit() {
+        return deposit;
     }
 
-    public void setPriceValue(BigDecimal priceValue) {
-        this.priceValue = priceValue;
+    public void setDeposit(BigDecimal deposit) {
+        this.deposit = deposit;
     }
 }
