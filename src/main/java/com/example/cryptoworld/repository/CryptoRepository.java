f9b7861481d6f99be93dfe0692c2191e71c896dc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-23 19:37:30 +0200

    crypto find by name repo method

--- src/main/java/com/example/cryptoworld/repository/CryptoRepository.java
+++ src/main/java/com/example/cryptoworld/repository/CryptoRepository.java
@@ -1,9 +1,14 @@
 package com.example.cryptoworld.repository;
 
 import com.example.cryptoworld.models.entities.CryptoCurrenciesEntity;
+import com.example.cryptoworld.models.enums.EnumCryptoTop10;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CryptoRepository extends JpaRepository<CryptoCurrenciesEntity, Long> {
+
+    Optional<CryptoCurrenciesEntity> findByCurrencies(EnumCryptoTop10 cryptoName);
 }
