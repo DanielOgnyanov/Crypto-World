4af731c5b5093fae6c9d9e2bf45b9c33d158155d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-24 18:22:05 +0200

    sell crypto binding model

--- src/main/java/com/example/cryptoworld/models/binding/SellCryptoBindingModel.java
+++ src/main/java/com/example/cryptoworld/models/binding/SellCryptoBindingModel.java
@@ -3,6 +3,11 @@ package com.example.cryptoworld.models.binding;
 
 import com.example.cryptoworld.models.enums.EnumCryptoTop10;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class SellCryptoBindingModel {
 
     private String usernameConfirm;
@@ -12,15 +17,20 @@ public class SellCryptoBindingModel {
     public SellCryptoBindingModel() {
     }
 
-
+    @NotNull(message = "Username cannot be empty !")
+    @NotEmpty(message = "Username cannot be empty !")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters !")
     public String getUsernameConfirm() {
         return usernameConfirm;
     }
+
 
     public void setUsernameConfirm(String usernameConfirm) {
         this.usernameConfirm = usernameConfirm;
     }
 
+    @NotNull(message = "Sell value cannot be empty !")
+    @DecimalMin(value = "0", message = "Sell value must be Positive !")
     public double getSellValue() {
         return sellValue;
     }
@@ -29,6 +39,7 @@ public class SellCryptoBindingModel {
         this.sellValue = sellValue;
     }
 
+    @NotNull(message = "Crypto card cannot be empty !")
     public EnumCryptoTop10 getCrypto() {
         return crypto;
     }
