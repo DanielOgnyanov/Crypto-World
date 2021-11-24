7dce459146a17cc5c63163f6ba116c9d9ed06cf5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-24 18:50:04 +0200

    sell controller set boolean check

--- src/main/java/com/example/cryptoworld/web/LogSellController.java
+++ src/main/java/com/example/cryptoworld/web/LogSellController.java
@@ -1,5 +1,7 @@
 package com.example.cryptoworld.web;
 
+import com.example.cryptoworld.models.binding.LogDepositBindingModel;
+import com.example.cryptoworld.models.binding.SellCryptoBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,19 @@ public class LogSellController {
     @GetMapping("/add")
     public String card(Model model) {
 
+        if (!model.containsAttribute("sellCryptoBindingModel")) {
+            model.addAttribute("sellCryptoBindingModel", new SellCryptoBindingModel());
+        }
+
+        if (!model.containsAttribute("logDepositBindingModel")) {
+            model.addAttribute("logDepositBindingModel", new LogDepositBindingModel());
+            model.addAttribute("userCheckIfIsPresent", false);
+        }
+
+        if (!model.containsAttribute("logDepositBindingModel")) {
+            model.addAttribute("logDepositBindingModel", new LogDepositBindingModel());
+            model.addAttribute("sellValueCheck", false);
+        }
 
         return "sell";
     }
