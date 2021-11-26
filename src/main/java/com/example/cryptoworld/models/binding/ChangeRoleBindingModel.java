6b441213e0ed09d142f61c8f01fd584d764ef2e1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-26 19:27:04 +0200

    change role binding model validation

--- src/main/java/com/example/cryptoworld/models/binding/ChangeRoleBindingModel.java
+++ src/main/java/com/example/cryptoworld/models/binding/ChangeRoleBindingModel.java
@@ -2,6 +2,10 @@ package com.example.cryptoworld.models.binding;
 
 import com.example.cryptoworld.models.enums.EnumRole;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class ChangeRoleBindingModel {
 
 
@@ -11,6 +15,9 @@ public class ChangeRoleBindingModel {
     public ChangeRoleBindingModel() {
     }
 
+    @NotNull(message = "Username cannot be empty !")
+    @NotEmpty(message = "Username cannot be empty !")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters !")
     public String getUsername() {
         return username;
     }
@@ -19,6 +26,7 @@ public class ChangeRoleBindingModel {
         this.username = username;
     }
 
+    @NotNull(message = "Role cannot be empty !")
     public EnumRole getEnumRole() {
         return enumRole;
     }
