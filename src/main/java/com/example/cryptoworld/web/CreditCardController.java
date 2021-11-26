54120f53956132620e1a9ce99d4d66da67a0fe3c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-26 19:24:22 +0200

    adding one card to users account method check

--- src/main/java/com/example/cryptoworld/web/CreditCardController.java
+++ src/main/java/com/example/cryptoworld/web/CreditCardController.java
@@ -3,6 +3,7 @@ package com.example.cryptoworld.web;
 import com.example.cryptoworld.models.binding.AddCreditCardBindingModel;
 import com.example.cryptoworld.models.service.AddCreditCardServiceModel;
 import com.example.cryptoworld.service.CreditCartService;
+import com.example.cryptoworld.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +21,23 @@ public class CreditCardController {
 
     private final ModelMapper modelMapper;
     private final CreditCartService creditCartService;
+    private final UserService userService;
 
-    public CreditCardController(ModelMapper modelMapper, CreditCartService creditCartService) {
+    public CreditCardController(ModelMapper modelMapper, CreditCartService creditCartService, UserService userService) {
         this.modelMapper = modelMapper;
         this.creditCartService = creditCartService;
+        this.userService = userService;
     }
 
     @GetMapping("/add")
     public String card(Model model) {
         if (!model.containsAttribute("addCreditCardBindingModel")) {
             model.addAttribute("addCreditCardBindingModel", new AddCreditCardBindingModel());
+        }
+
+        if (!model.containsAttribute("addCreditCardBindingModel")) {
+            model.addAttribute("addCreditCardBindingModel", new AddCreditCardBindingModel());
+            model.addAttribute("addOneCardCheck", false);
         }
         return "card";
     }
@@ -45,6 +53,13 @@ public class CreditCardController {
             redirectAttributes.addFlashAttribute("addCreditCardBindingModel", addCreditCardBindingModel);
             redirectAttributes.addFlashAttribute(
                     "org.springframework.validation.BindingResult.addCreditCardBindingModel", bindingResult);
+
+            return "redirect:add";
+        }
+
+        if (userService.creditCardCheckIfIsPresent(userService.checkUsernameOfLoggedUser())) {
+            redirectAttributes.addFlashAttribute("addCreditCardBindingModel", addCreditCardBindingModel);
+            redirectAttributes.addFlashAttribute("addOneCardCheck", true);
 
             return "redirect:add";
         }
