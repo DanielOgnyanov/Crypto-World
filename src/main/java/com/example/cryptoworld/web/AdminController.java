4b398f905d37c5536aa069031f62e812cb19ac4a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-11-27 16:48:35 +0200

    admin html initial set to view li

--- src/main/java/com/example/cryptoworld/web/AdminController.java
+++ src/main/java/com/example/cryptoworld/web/AdminController.java
@@ -1,5 +1,7 @@
 package com.example.cryptoworld.web;
 
+import com.example.cryptoworld.models.binding.ChangeRoleBindingModel;
+import com.example.cryptoworld.models.binding.UserRegistrationBindingModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,13 @@ public class AdminController {
     @GetMapping("/setting")
 
     public String adminPanel(Model model) {
+
+        if (!model.containsAttribute("changeRoleBindingModel")) {
+            model.addAttribute("changeRoleBindingModel", new ChangeRoleBindingModel());
+        }
+
+
+
         return "admin";
     }
 }
