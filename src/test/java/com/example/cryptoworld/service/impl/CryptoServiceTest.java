1729404873542ac098d8564b2f3fec1c2a0cb72a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-05 16:36:31 +0200

    crypto service unit testing

--- src/test/java/com/example/cryptoworld/service/impl/CryptoServiceTest.java
+++ src/test/java/com/example/cryptoworld/service/impl/CryptoServiceTest.java
@@ -1,12 +1,20 @@
 package com.example.cryptoworld.service.impl;
 
 import com.example.cryptoworld.models.entities.CryptoCurrenciesEntity;
+import com.example.cryptoworld.models.enums.EnumCryptoTop10;
 import com.example.cryptoworld.repository.CryptoRepository;
+import com.example.cryptoworld.service.CryptoService;
 import com.example.cryptoworld.service.Impl.CryptoServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -22,6 +30,27 @@ public class CryptoServiceTest {
     @BeforeEach
     public void init() {
 
+        this.cryptoCurrenciesEntity = new CryptoCurrenciesEntity();
+        this.cryptoCurrenciesEntity.setCurrencies(EnumCryptoTop10.Bitcoin);
+        this.cryptoCurrenciesEntity.setPrice(66000);
+
+        this.cryptoRepository = Mockito.mock(CryptoRepository.class);
+    }
+
+
+
+    @Test
+    public void testInitCryptoInDb() {
+
+        Mockito.when(cryptoRepository.findAll()).thenReturn(List.of(this.cryptoCurrenciesEntity));
+
+        CryptoService cryptoService = new CryptoServiceImpl(this.cryptoRepository, new ModelMapper());
+
+
+        cryptoService.InitCryptoInDb();
+
+
+        Assertions.assertEquals(1, this.cryptoRepository.findAll().size());
     }
 
 
