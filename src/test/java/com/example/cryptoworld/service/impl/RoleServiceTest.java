e21743401ece68620917a0c85848853920eb1c34
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-05 16:55:41 +0200

    role service unit testing

--- src/test/java/com/example/cryptoworld/service/impl/RoleServiceTest.java
+++ src/test/java/com/example/cryptoworld/service/impl/RoleServiceTest.java
@@ -5,13 +5,17 @@ import com.example.cryptoworld.models.entities.RoleEntity;
 import com.example.cryptoworld.models.enums.EnumRole;
 import com.example.cryptoworld.repository.RoleRepository;
 import com.example.cryptoworld.service.Impl.RoleServiceImpl;
+import com.example.cryptoworld.service.RoleService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.management.relation.Role;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class RoleServiceTest {
@@ -32,6 +36,21 @@ public class RoleServiceTest {
         this.roleEntity.setRole(EnumRole.ADMIN);
 
         this.roleRepository = Mockito.mock(RoleRepository.class);
+
+    }
+
+
+    @Test
+    public void testInitRoleInDb() {
+
+        Mockito.when(roleRepository.findAll()).thenReturn(List.of(this.roleEntity));
+
+        RoleService roleService = new RoleServiceImpl(this.roleRepository);
+
+        roleService.InitRoleInDb();
+
+        Assertions.assertEquals(1, this.roleRepository.findAll().size());
+
 
     }
 }
