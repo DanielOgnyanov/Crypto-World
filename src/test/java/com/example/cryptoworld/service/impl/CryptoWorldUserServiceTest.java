ce44df126ac6a3f497d119f5303236a7ae846cb1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-06 18:57:53 +0200

    crypto world user service test

--- src/test/java/com/example/cryptoworld/service/impl/CryptoWorldUserServiceTest.java
+++ src/test/java/com/example/cryptoworld/service/impl/CryptoWorldUserServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.cryptoworld.service.impl;
 
+import com.example.cryptoworld.models.entities.RoleEntity;
+import com.example.cryptoworld.models.entities.UserEntity;
+import com.example.cryptoworld.models.enums.EnumRole;
 import com.example.cryptoworld.repository.UserRepository;
 import com.example.cryptoworld.service.Impl.CryptoWorldUserService;
 import org.junit.jupiter.api.Assertions;
@@ -10,7 +13,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 public class CryptoWorldUserServiceTest {
@@ -34,6 +41,32 @@ public class CryptoWorldUserServiceTest {
                     cryptoWorldUserService.loadUserByUsername("user_does_not_exits");
                 }
         );
+    }
+
+
+    @Test
+    void testExistingUser() {
+
+        UserEntity userEntity = new UserEntity();
+
+        userEntity.setUsername("Check");
+        userEntity.setPassword("12345");
+
+        RoleEntity roleEntity = new RoleEntity();
+        roleEntity.setRole(EnumRole.USER);
+
+        userEntity.setRoles(List.of(roleEntity));
+
+
+        Mockito.when(userRepository.findByUsername("Check")).thenReturn(Optional.of(userEntity));
+
+
+        UserDetails userDetails = cryptoWorldUserService.loadUserByUsername("Check");
+
+
+        Assertions.assertEquals(userEntity.getUsername(), userDetails.getUsername());
+
+
     }
 
 }
