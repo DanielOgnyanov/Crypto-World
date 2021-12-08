f31bd9616b5f08a86c8512e8e65171c25bddbaf0
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-08 21:05:05 +0200

    scheduling method to delete lowest log sell

--- src/main/java/com/example/cryptoworld/config/SchedulingConfig.java
+++ src/main/java/com/example/cryptoworld/config/SchedulingConfig.java
@@ -2,6 +2,7 @@ package com.example.cryptoworld.config;
 
 
 import com.example.cryptoworld.service.LogDepositService;
+import com.example.cryptoworld.service.LogSellService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +10,17 @@ import org.springframework.stereotype.Component;
 public class SchedulingConfig {
 
     private final LogDepositService logDepositService;
+    private final LogSellService logSellService;
 
-    public SchedulingConfig(LogDepositService logDepositService) {
+    public SchedulingConfig(LogDepositService logDepositService, LogSellService logSellService) {
         this.logDepositService = logDepositService;
+        this.logSellService = logSellService;
     }
 
 
     @Scheduled(cron = "0 0 0 25 12 ?")
     public void deleteLogDeposit(){
         logDepositService.deleteLowestDeposit();
+        logSellService.deleteLowestSell();
     }
 }
