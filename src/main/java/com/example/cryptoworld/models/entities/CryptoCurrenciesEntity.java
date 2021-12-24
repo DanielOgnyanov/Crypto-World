a6488590593f70d0aae0a78cb62f9b8202501471
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-24 16:43:06 +0200

    adding 24 h volume data from api call

--- src/main/java/com/example/cryptoworld/models/entities/CryptoCurrenciesEntity.java
+++ src/main/java/com/example/cryptoworld/models/entities/CryptoCurrenciesEntity.java
@@ -4,6 +4,7 @@ package com.example.cryptoworld.models.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "crypto_currencies")
@@ -11,15 +12,17 @@ public class CryptoCurrenciesEntity extends BaseEntity{
 
     private String name;
     private String assetStringId;
+    private BigDecimal volumeFor24Hour;
     private double price;
 
 
     public CryptoCurrenciesEntity() {
     }
 
-    public CryptoCurrenciesEntity(String name, String assetStringId, double price) {
+    public CryptoCurrenciesEntity(String name, String assetStringId, BigDecimal volumeFor24Hour, double price) {
         this.name = name;
         this.assetStringId = assetStringId;
+        this.volumeFor24Hour = volumeFor24Hour;
         this.price = price;
     }
 
@@ -44,6 +47,15 @@ public class CryptoCurrenciesEntity extends BaseEntity{
 
     public void setAssetStringId(String assetStringId) {
         this.assetStringId = assetStringId;
+    }
+
+    @Column(name = "volume_for_24_hour")
+    public BigDecimal getVolumeFor24Hour() {
+        return volumeFor24Hour;
+    }
+
+    public void setVolumeFor24Hour(BigDecimal volumeFor24Hour) {
+        this.volumeFor24Hour = volumeFor24Hour;
     }
 
     @Column(name = "price")
