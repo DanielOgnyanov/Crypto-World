792c17f671a4d6c8c90198c6b9196f8b76282519
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2021-12-24 17:43:49 +0200

    chenge name of enum crypto

--- src/main/java/com/example/cryptoworld/models/enums/EnumCryptoTop10.java
+++ src/main/java/com/example/cryptoworld/models/enums/EnumCryptoTop10.java
@@ -1,28 +1,16 @@
 package com.example.cryptoworld.models.enums;
 
-import java.math.BigDecimal;
-
 public enum EnumCryptoTop10 {
 
-    Bitcoin(66000),
-    Ethereum(4700),
-    Binance(642),
-    Cardano(2.28),
-    Tether(1),
-    Solana(240),
-    XRP(1.26),
-    Polkadot(51),
-    Dogecoin(0.27),
-    Usdcoin(1);
-
-
-    private double price;
-
-    EnumCryptoTop10(double price) {
-        this.price = price;
-    }
-
-    public double getPrice() {
-        return price;
-    }
+    Bitcoin,
+    Ethereum,
+    Binance,
+    Cardano,
+    Tether,
+    SOL,
+    Ripple,
+    Dotcoin,
+    DogeCoin,
+    USDC;
+    
 }
