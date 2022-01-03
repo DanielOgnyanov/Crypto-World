d502643cb868b9646e451c21d05013ce97153db6
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-03 21:29:50 +0200

    user navigation

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -6,17 +6,32 @@ const Header = () => {
 
     let guestNavigation = (
         <div id="guest">
-            <Link className="button" to="/login">Sign In</Link>
+            <Link to="/login"    className="button">Sign In</Link>
             <Link to="/register" className="button">Create Account</Link>
         </div>
     );
 
 
+    let userNavigation = (
+        <div id="user">
+            <span>Welcome, Ivan </span>
+            <Link className="button" to="/">Home</Link>
+            <Link className="button" to="/create">Add Credit Card</Link>
+            <Link className="button" to="/logout">Logout</Link>
+        </div>
+    );
 
     return (
         <header id="site-header">
              <nav className="navbar">
 
+             <section className="navbar-dashboard">
+             
+                         userNavigation
+                         guestNavigation
+                    
+             
+             </section>
              
        
              </nav>
