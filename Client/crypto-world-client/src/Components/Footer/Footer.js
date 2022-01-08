f7247b9b82fbc436b3b12490baa41dd7b60b092c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-08 18:32:23 +0200

    footer content css

--- Client/crypto-world-client/src/Components/Footer/Footer.js
+++ Client/crypto-world-client/src/Components/Footer/Footer.js
@@ -4,7 +4,15 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer >
-            <p> &copy;Copyright 2021 Crypto World, Inc.</p>
+            
+           <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">Home</a></li>
+                <li class="list-inline-item"><a href="#">Services</a></li>
+                <li class="list-inline-item"><a href="#">About</a></li>
+                <li class="list-inline-item"><a href="#">Terms</a></li>
+                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            </ul>
+            <p class="copyright">&copy;Copyright 2021 Crypto World, Inc.</p>
         </footer>
     );
 };
