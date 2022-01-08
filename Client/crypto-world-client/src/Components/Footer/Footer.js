75993147033d59374e8a9c4edd0661d6900e2539
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-08 18:53:58 +0200

    footer style css and li elements

--- Client/crypto-world-client/src/Components/Footer/Footer.js
+++ Client/crypto-world-client/src/Components/Footer/Footer.js
@@ -4,13 +4,13 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer >
-            
-           <ul class="list-inline">
-                <li class="list-inline-item"><a href="#">Home</a></li>
-                <li class="list-inline-item"><a href="#">Services</a></li>
-                <li class="list-inline-item"><a href="#">About</a></li>
-                <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+            <p>Crypto Wolrd</p>
+           <ul >
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Terms</li>
+                <li>Privacy Policy</li>
             </ul>
             <p class="copyright">&copy;Copyright 2021 Crypto World, Inc.</p>
         </footer>
