24fe5f0268529adafe069cdb1f18999627b19e9a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-11 21:45:44 +0200

    Change app router element

--- Client/crypto-world-client/src/App.js
+++ Client/crypto-world-client/src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Route, Routes, Redirect } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Register from './Components/Register/Register';
@@ -11,9 +11,14 @@ function App() {
   return (
     <div className='container' >
       <Header/>
+      
       <Routes>
-      <Route path="/register" component={Register} />
+
+       <Route path = "/register" element = {<Register/>}/>
+
+
       </Routes>
+     
       
       <Footer/>
      
