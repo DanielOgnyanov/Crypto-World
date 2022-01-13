46aa834e12c602f46c0af28a3f1314681baab9ff
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-13 21:50:32 +0200

    Register form

--- Client/crypto-world-client/src/Components/Register/Register.js
+++ Client/crypto-world-client/src/Components/Register/Register.js
@@ -6,16 +6,36 @@ import './Register.css'
 
 const Register = () => {
 
-    
-    
-
-    return (
 
 
-      <h3>Register</h3>
+
+  return (
 
 
-    );
+    <form>
+      <p for="name">Create Account</p>
+      <div >
+      <span className='icon'><i className='fa fa-user icon'></i></span>
+      <label for="username" >Username</label>
+      <input name='username' placeholder='Username' type="text"></input>
+      
+      </div>
+      
+      <label for="fullname">Full Name</label>
+      <input name='fullname' placeholder='Full Name' type="text"></input>
+      
+      <label for="email">Email</label>
+      <input name='email' placeholder='Email' type="text"></input>
+      
+      <label for="password">Password</label>
+      <input name='password' placeholder='Password' type="password"></input>
+      
+      <label for="confirmPassword">Confirm Password</label>
+      <input name='confirmPassword' placeholder='Confirm Password' type="password"></input>
+    </form>
+
+
+  );
 }
 
 export default Register;
