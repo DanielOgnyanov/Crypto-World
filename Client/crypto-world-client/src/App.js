eaacb069d21bec1f77fb97e3f71749dd4ddb055b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-18 19:06:37 +0200

    adding route to get started div

--- Client/crypto-world-client/src/App.js
+++ Client/crypto-world-client/src/App.js
@@ -5,28 +5,28 @@ import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login'
-import GetStartedCard from './Components/GetStartedCard/GetStartedCard'
+import GetStarted from './Components/GetStarted/GetStarted'
 
 
 
 function App() {
   return (
     <div className='container' >
-      <Header/>
-      
-      <Routes>
+      <Header />
 
-       <Route path = "/register" element = {<Register/>}/>
-       <Route path = "/login" element = {<Login/>}/>
-       <Route path = "/register" element = {<GetStartedCard/>}/>
+      <Routes>
+        <Route exact path="/" exact element={<GetStarted />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+
 
 
 
       </Routes>
-     
-      
-      <Footer/>
-     
+
+
+      <Footer />
+
     </div>
   );
 }
