0a679a18fa5d4f5ac00446fa158fd962abd62eda
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-18 19:48:03 +0200

    button style nad position

--- Client/crypto-world-client/src/Components/GetStarted/GetStarted.js
+++ Client/crypto-world-client/src/Components/GetStarted/GetStarted.js
@@ -1,4 +1,5 @@
 import './GetStarted.css'
+import { Link } from 'react-router-dom'
 
 
 
@@ -11,14 +12,20 @@ const GetStarted = () => {
 
 
         <div className='getStarted' id='getStarted-div'>
-         <h1>Buy Bitcoin & Crypto Asset</h1>
-         <p>Sign up today and 
-             <strong> buy top 10 cryptocurrencies</strong> , 
-             <br/>
-             easly and fast. Get started with as little as 
-             <strong> $10.</strong>
-             
-             </p>
+
+            <h1>Buy Bitcoin & Crypto Asset</h1>
+
+            <p>Sign up today and
+                <strong> buy top 10 cryptocurrencies</strong> ,
+                <br />
+                easly and fast. Get started with as little as
+                <strong> $10.</strong>
+
+            </p>
+            <div className='getStartedButton' id='getStartedButton-div'>
+
+                <Link className='button' to="/register">Get Started</Link>
+            </div>
 
 
 
