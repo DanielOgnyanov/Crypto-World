462056b76bdb8c45b0cf9a04bf0e33380d53eb56
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-18 18:17:42 +0200

    route with login component

--- Client/crypto-world-client/src/App.js
+++ Client/crypto-world-client/src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Register from './Components/Register/Register';
+import Login from './Components/Login/Login'
 import GetStartedCard from './Components/GetStartedCard/GetStartedCard'
 
 
@@ -16,7 +17,9 @@ function App() {
       <Routes>
 
        <Route path = "/register" element = {<Register/>}/>
+       <Route path = "/login" element = {<Login/>}/>
        <Route path = "/register" element = {<GetStartedCard/>}/>
+
 
 
       </Routes>
