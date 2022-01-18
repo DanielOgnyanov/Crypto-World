1d03379c046cd4f100678f16a3fb0244b581b11c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-18 19:27:11 +0200

    get started div style and text

--- Client/crypto-world-client/src/Components/GetStarted/GetStarted.js
+++ Client/crypto-world-client/src/Components/GetStarted/GetStarted.js
@@ -10,8 +10,15 @@ const GetStarted = () => {
     return (
 
 
-        <div>
-         <h1>Buy Bitcoin And Crypto Asset</h1>
+        <div className='getStarted' id='getStarted-div'>
+         <h1>Buy Bitcoin & Crypto Asset</h1>
+         <p>Sign up today and 
+             <strong> buy top 10 cryptocurrencies</strong> , 
+             <br/>
+             easly and fast. Get started with as little as 
+             <strong> $10.</strong>
+             
+             </p>
 
 
 
