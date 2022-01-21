360f8f7f10fe2dceef794516f4918f6d72bd2a60
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-21 18:20:17 +0200

    login function

--- Client/crypto-world-client/src/Components/Login/Login.js
+++ Client/crypto-world-client/src/Components/Login/Login.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
 import './Login.css'
 
 
 
 const Login = () => {
 
+    const [username ,setUsername] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const history = useNavigate();
+
+    function login() {
+
+        history('/home');
+    }
 
 
+
+    function login()  {
+
+        console.log(username,password);
+
+    }
 
     return (
 
@@ -15,13 +32,15 @@ const Login = () => {
 
 
             <label for="username" >Username</label>
-            <input name='username' placeholder='Username' type="text" id='text1'></input>
+            <input name='username' placeholder='Username' type="text" id='text1' 
+            onChange={(e) => setUsername(e.target.value)}/>
 
 
             <label for="password">Password</label>
-            <input name='password' placeholder='Password' type="password" id='text4'></input>
+            <input name='password' placeholder='Password' type="password" id='text4'
+            onChange={(e) => setPassword(e.target.value)}/>
 
-            <button>Sign In</button>
+            <button onClick={login}>Sign In</button>
         </form>
 
 
