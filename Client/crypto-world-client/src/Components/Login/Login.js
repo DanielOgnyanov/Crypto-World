f2b305b4dc55765c89461e5325dd5420aeec02a4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-21 18:46:07 +0200

    login function after submit url

--- Client/crypto-world-client/src/Components/Login/Login.js
+++ Client/crypto-world-client/src/Components/Login/Login.js
@@ -6,39 +6,34 @@ import './Login.css'
 
 const Login = () => {
 
-    const [username ,setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
-  
+
     const history = useNavigate();
+
+
 
     function login() {
 
         history('/home');
-    }
-
-
-
-    function login()  {
-
-        console.log(username,password);
-
+        console.log(username);
     }
 
     return (
 
 
         <form id='login-form'>
-            <p for="name">Sign In</p>
+            <p htmlFor="name">Sign In</p>
 
 
-            <label for="username" >Username</label>
-            <input name='username' placeholder='Username' type="text" id='text1' 
-            onChange={(e) => setUsername(e.target.value)}/>
+            <label htmlFor="username" >Username</label>
+            <input name='username' placeholder='Username' type="text" id='text1'
+                onChange={(e) => setUsername(e.target.value)} />
 
 
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input name='password' placeholder='Password' type="password" id='text4'
-            onChange={(e) => setPassword(e.target.value)}/>
+                onChange={(e) => setPassword(e.target.value)} />
 
             <button onClick={login}>Sign In</button>
         </form>
