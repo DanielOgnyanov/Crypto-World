2bcec0017709d9b5d5153e161a15e86edc9d6b21
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-27 21:54:21 +0200

    login service

--- Client/crypto-world-client/src/Components/Services/authService.js
+++ Client/crypto-world-client/src/Components/Services/authService.js
@@ -1,12 +1,12 @@
 const baseUrl = 'http://localhost:5000';
 
-export const login = async (email, password) => {
+export const login = async (username, password) => {
     let res = await fetch(`${baseUrl}/users/login`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ username, password })
     });
 
     let jsonResult = await res.json();
