d9baf9c5f38f9651062cd0c15f13038cd43f9cde
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-01-30 19:17:12 +0200

    info utils

---
+++ Server/src/main/java/com/example/cryptoworld/config/InfoUtils.java
@@ -0,0 +1,30 @@
+package com.example.cryptoworld.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class InfoUtils {
+
+    private  String API_URL =
+            "https://rest.coinapi.io/v1/assets?filter_asset_id=BTC;ETH;BNB;USDT;SOL;XRP;ADA;USDC;DOT;DOGE";
+
+    private  String API_NAME = "X-CoinAPI-Key";
+
+    private  String PRIVATE_KEY = "C32D27DA-45FA-4F89-9F2A-68F45DEB5A1F";
+
+
+    public InfoUtils() {
+    }
+
+    public String getAPI_URL() {
+        return API_URL;
+    }
+
+    public String getAPI_NAME() {
+        return API_NAME;
+    }
+
+    public String getPRIVATE_KEY() {
+        return PRIVATE_KEY;
+    }
+}
