c20be97216e748a86df133deb60b5266c0e066b5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-02-08 19:39:34 +0200

    header  user and guest nav

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -2,19 +2,53 @@ import './Header.css'
 import logoImg from "../../Images/logo-nav-2.png"
 import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
+import { useAuthContext } from '../../Context/AuthContext';
 
 
 const Header = () => {
-    
-    const {isAuthenticated, username} = useContext(AuthContext);
+
+    const { user } = useAuthContext();
 
     const history = useNavigate();
-    
+
 
     function handleClick() {
 
         history('/register');
     }
+
+    let userNavigation = (
+
+
+        <div className='userNavigation'>
+
+            <Link className='button' to="/home" data-toggle="collapse">Home</Link>
+            <Link className='button' to="#" data-toggle="collapse">Admin</Link>
+
+            <Link className='button' to="#">Add Credit Card</Link>
+            <Link className='button' to="#">Buy Crypto</Link>
+            <Link className='button' to="#">Sell Crypto</Link>
+
+        </div>
+
+    );
+
+
+    let guestNavigation = (
+
+        <div className='guestNavigation'>
+
+            <Link className='button' to="/login">Sign In</Link>
+
+            <button onClick={handleClick} type='submit'>Create Account</button>
+
+
+        </div>
+
+
+
+
+    );
 
 
 
@@ -27,31 +61,13 @@ const Header = () => {
                 <Link className='button' to="/" ><img src={logoImg} alt=""></img></Link>
 
 
-
-                <div className='userNavigation'>
-
-                    <Link className='button' to="/home" data-toggle="collapse">Home</Link>
-                    <Link className='button' to="#" data-toggle="collapse">Admin</Link>
-
-                    <Link className='button' to="#">Add Credit Card</Link>
-                    <Link className='button' to="#">Buy Crypto</Link>
-                    <Link className='button' to="#">Sell Crypto</Link>
-
-                </div>
-
-                <div className='guestNavigation'>
-
-                    <Link className='button' to="/login">Sign In</Link>
-
-                    <button onClick={handleClick} type='submit'>Create Account</button>
-
-
-                </div>
-
-
-
-
-
+                { user.username
+                
+                ? userNavigation
+                : guestNavigation
+                
+                
+                }
 
 
             </nav>
