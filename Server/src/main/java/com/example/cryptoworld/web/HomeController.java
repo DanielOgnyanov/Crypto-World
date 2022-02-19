cab2cb61b4958f89ce60d949d9867a846c122db2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-02-19 19:17:11 +0200

    home controller adding get crypto method

--- Server/src/main/java/com/example/cryptoworld/web/HomeController.java
+++ Server/src/main/java/com/example/cryptoworld/web/HomeController.java
@@ -1,17 +1,19 @@
 package com.example.cryptoworld.web;
 
 
+import com.example.cryptoworld.models.view.CryptoViewModel;
 import com.example.cryptoworld.service.CreditCartService;
 import com.example.cryptoworld.service.CryptoService;
 import com.example.cryptoworld.service.WalletService;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/home/")
+@RequestMapping("/api/home")
 public class HomeController {
 
     private final WalletService walletService;
@@ -26,6 +28,12 @@ public class HomeController {
 
 
 
+    @GetMapping("/cryptoPrice")
+    public List<CryptoViewModel> getCryptoPrice(){
+
+
+        return cryptoService.getAllCrypto();
+    }
 
     @GetMapping("/")
     public String index() {
