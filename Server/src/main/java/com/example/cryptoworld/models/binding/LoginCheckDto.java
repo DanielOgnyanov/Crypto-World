0ea07df858ca8050c5f35f83dbbf480d9c4cb70d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-02-21 19:07:50 +0200

    login check dto

---
+++ Server/src/main/java/com/example/cryptoworld/models/binding/LoginCheckDto.java
@@ -0,0 +1,14 @@
+package com.example.cryptoworld.models.binding;
+
+public class LoginCheckDto {
+
+    private String username;
+
+
+    public LoginCheckDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
