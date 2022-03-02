362d42ff4f1055c111c81f19626baf273cd144c4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-03-02 19:48:51 +0200

    import chart in homepage

--- Client/crypto-world-client/src/Components/HomePage/HomePage.js
+++ Client/crypto-world-client/src/Components/HomePage/HomePage.js
@@ -1,5 +1,6 @@
 import './HomePage.css'
 import * as cryptoService from '../../Services/CryptoService'
+import Chart from '../Chart/Chart';
 
 
 
@@ -7,9 +8,9 @@ const HomePage = () => {
 
     const cryptoPrices = cryptoService.getCryptoPrice;
 
-    const cryptoItem = 
-    [{ id: 1, name: "Bitcoin", price: 45000 },
-     { id: 2, name: "Ethereum", price: 3000}];
+    const cryptoItem =
+        [{ id: 1, name: "Bitcoin", price: 45000 },
+        { id: 2, name: "Ethereum", price: 3000 }];
 
 
 
@@ -22,30 +23,37 @@ const HomePage = () => {
     return (
 
 
-        <table className="table">
-            <thead className="table-light">
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Crypto Name</th>
-                    <th scope="col">Current Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {cryptoItem.map(Item => (
 
+
+        <div className='home'>
+            <div className='chart'>
+                <Chart></Chart>
+            </div>
+
+            <table className="table">
+                <thead className="table-light">
                     <tr>
-                        <th scope="row" >{Item.id}</th>
-                        <td >{Item.name}</td>
-                        <td >{Item.price}</td>
+                        <th scope="col">#</th>
+                        <th scope="col">Crypto Name</th>
+                        <th scope="col">Current Price</th>
                     </tr>
+                </thead>
+                <tbody>
+                    {cryptoItem.map(Item => (
+
+                        <tr>
+                            <th scope="row" >{Item.id}</th>
+                            <td >{Item.name}</td>
+                            <td >{Item.price}</td>
+                        </tr>
 
 
-                ))}
+                    ))}
 
-            </tbody>
-        </table>
+                </tbody>
+            </table>
 
-
+        </div>
 
 
 
