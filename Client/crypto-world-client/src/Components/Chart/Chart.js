3011943682557c5d38fd68e29f0e4f81c99378ce
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-03-03 20:46:24 +0200

    chart setup

--- Client/crypto-world-client/src/Components/Chart/Chart.js
+++ Client/crypto-world-client/src/Components/Chart/Chart.js
@@ -3,19 +3,31 @@ import { Doughnut } from 'react-chartjs-2'
 
 const Chart = () => {
 
+    const data = {
+        labels: [
+          'Red',
+          'Blue',
+          'Yellow'
+        ],
+        datasets: [{
+          label: 'Dataset',
+          data: [300, 50, 100],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)'
+          ],
+          hoverOffset: 4
+        }]
+      };
+
 
     return (
 
 
         <div>
             <Doughnut>
-                data{{
-                    labels: [
-                        'Red',
-                        'Blue',
-                        'Yellow'
-                      ]
-                }}
+       
 
 
             </Doughnut>
