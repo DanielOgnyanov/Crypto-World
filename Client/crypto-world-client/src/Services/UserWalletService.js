66ab152aba6182fc2d6e20860fbf3ceb911aa97b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-03-14 21:19:04 +0200

    get user wallet method

--- Client/crypto-world-client/src/Services/UserWalletService.js
+++ Client/crypto-world-client/src/Services/UserWalletService.js
@@ -1,2 +1,19 @@
 const baseUrl = 'http://localhost:8000';
 
+export const getUserWallet = async () => {
+    let res = await fetch(`${baseUrl}/api/user/wallet`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify()
+    });
+
+    let jsonResult = await res.json();
+
+    if (res.ok) {
+        return jsonResult;
+    } else {
+        throw jsonResult.message;
+    }
+};
