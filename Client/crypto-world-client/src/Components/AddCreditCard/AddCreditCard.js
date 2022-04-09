23ed3a8ba4b028b074bf399a77b213e985b8292d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-04-09 20:09:01 +0300

    balance label in credit card component

--- Client/crypto-world-client/src/Components/AddCreditCard/AddCreditCard.js
+++ Client/crypto-world-client/src/Components/AddCreditCard/AddCreditCard.js
@@ -19,7 +19,7 @@ const AddCreditCard = () => {
             <label htmlFor="expiration-year">Expiration Year</label>
             <input name='expiration-year' autoComplete="on" placeholder='Expiration Year' type="number" id='expiration-year' />
 
-            
+            <label htmlFor="balance">Balance</label>
 
             
 
