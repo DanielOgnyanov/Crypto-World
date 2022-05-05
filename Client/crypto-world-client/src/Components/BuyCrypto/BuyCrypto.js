23f26ae01c2b06517b6a14f31ad1f8d6ab663e9d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-05-05 20:23:55 +0300

    username confirm input and css

--- Client/crypto-world-client/src/Components/BuyCrypto/BuyCrypto.js
+++ Client/crypto-world-client/src/Components/BuyCrypto/BuyCrypto.js
@@ -14,6 +14,7 @@ const BuyCrypto = () => {
         <form id='buy-crypto-form'>
 
             <label htmlFor='Username Confirm'></label>
+            <input name='username-confirm' autoComplete="on" placeholder='username-confirm' type="text" id='username-confirm' />
 
 
 
