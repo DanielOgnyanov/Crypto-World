7395e0e95647be4ce4cfc53e85a89635ba10f2e1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-05-17 20:32:41 +0300

    input username confirm

--- Client/crypto-world-client/src/Components/SellCrypto/SellCrypto.js
+++ Client/crypto-world-client/src/Components/SellCrypto/SellCrypto.js
@@ -13,6 +13,9 @@ const SellCrypto = () => {
        <form>
            <label htmlFor='username-confirm'>Username Confirm</label>
 
+           <input name='username-confirm' autoComplete="on" placeholder='Username Confirm'
+                type="text" id='username-confirm' />
+
 
 
 
