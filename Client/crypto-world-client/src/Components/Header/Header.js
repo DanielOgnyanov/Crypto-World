f92e1157b4fcc9a714c2e8746fbbf4a98dd7903b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-05-24 20:54:16 +0300

    admin import in header

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -55,11 +55,12 @@ const Header = () => {
                 <div className='userNavigation'>
 
                     <Link className='button' to="/home" data-toggle="collapse">Home</Link>
-                    <Link className='button' to="#" data-toggle="collapse">Admin</Link>
+                    <Link className='button' to="/admin" data-toggle="collapse">Admin</Link>
 
                     <Link className='button' to="/add-credit-card">Add Credit Card</Link>
                     <Link className='button' to="/buy-crypto">Buy Crypto</Link>
                     <Link className='button' to="sell-crypto">Sell Crypto</Link>
+                    
 
                 </div>
 
