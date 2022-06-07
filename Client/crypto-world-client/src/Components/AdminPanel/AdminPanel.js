cd3708563f0cc4e87164dae9effc1257b066c60b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-06-07 18:09:40 +0300

    admin panel change

--- Client/crypto-world-client/src/Components/AdminPanel/AdminPanel.js
+++ Client/crypto-world-client/src/Components/AdminPanel/AdminPanel.js
@@ -8,14 +8,7 @@ const AdminPanel = () => {
 
     return (
 
-        <form id="last-buy">
-            <h3>Last buy</h3>
-            <h3>Last sell</h3>
-
-
-
-
-        </form>
+       
         
 
 
