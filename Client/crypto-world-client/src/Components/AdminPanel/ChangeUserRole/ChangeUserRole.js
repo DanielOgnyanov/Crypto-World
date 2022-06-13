a6f805b4935cbf0fe527ec519c82b7d4f27d93b4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-06-13 19:38:36 +0300

    change role username field

--- Client/crypto-world-client/src/Components/AdminPanel/ChangeUserRole/ChangeUserRole.js
+++ Client/crypto-world-client/src/Components/AdminPanel/ChangeUserRole/ChangeUserRole.js
@@ -8,7 +8,16 @@ const ChangeUserRole = () => {
 
     return (
 
-       
+       <form>
+        <label htmlFor="Username" >Username</label>
+        <input name='username' autoComplete="on" placeholder='Username' type="text" id='login-username' />
+
+
+
+
+
+
+       </form>
         
 
 
