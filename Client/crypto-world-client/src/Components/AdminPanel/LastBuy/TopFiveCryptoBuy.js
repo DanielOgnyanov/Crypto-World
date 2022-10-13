98691d754ebe281ae8775e034b1a83b090a115a4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-10-13 20:43:56 +0300

    top five user buy form init for username

--- Client/crypto-world-client/src/Components/AdminPanel/LastBuy/TopFiveCryptoBuy.js
+++ Client/crypto-world-client/src/Components/AdminPanel/LastBuy/TopFiveCryptoBuy.js
@@ -9,7 +9,7 @@ const LastBuy = () => {
         <p>Top 5 Crypto Buy</p>
 
         <label htmlFor="Username" >Username</label>
-        
+        <input name='username' autoComplete="on" placeholder='Username' type="text"/>
 
 
     </form>
