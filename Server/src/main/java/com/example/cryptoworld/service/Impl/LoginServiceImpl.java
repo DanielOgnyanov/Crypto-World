f60f253b42835a4fd8e315ecde005622bfbd12d4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-07 20:55:23 +0200

    login service init

--- Server/src/main/java/com/example/cryptoworld/service/Impl/LoginServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/LoginServiceImpl.java
@@ -1,10 +1,23 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.UserRepository;
 import com.example.cryptoworld.service.LoginService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginServiceImpl implements LoginService {
+
+    private final UserRepository userRepository;
+
+    public LoginServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public void setUserLoginInDb(String username) {
+
+
 
     }
 }
