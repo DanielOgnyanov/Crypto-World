9f36d55da8564230108d1dce144acec53f2a85bd
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-08 21:33:20 +0200

    add login service in user controller

--- Server/src/main/java/com/example/cryptoworld/web/UserController.java
+++ Server/src/main/java/com/example/cryptoworld/web/UserController.java
@@ -97,6 +97,8 @@ public class UserController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
 
+        loginService.setUserLoginInDb(userRegistrationDto.getUsername(),true);
+
         return new ResponseEntity<>("New user registered", HttpStatus.OK);
 
     }
