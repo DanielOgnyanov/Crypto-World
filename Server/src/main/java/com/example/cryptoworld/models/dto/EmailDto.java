7b35f3028b8e6a7c62538c930c77b37caa9caef8
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-12 21:58:55 +0200

    email dto

---
+++ Server/src/main/java/com/example/cryptoworld/models/dto/EmailDto.java
@@ -0,0 +1,17 @@
+package com.example.cryptoworld.models.dto;
+
+public class EmailDto {
+
+    private String email;
+
+    public EmailDto() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
