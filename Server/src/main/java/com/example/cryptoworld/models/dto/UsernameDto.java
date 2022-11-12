256e8b60d68dc4d7c4c8025ee864998ebfcbbb1b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-12 16:22:47 +0200

    username dto

--- Server/src/main/java/com/example/cryptoworld/models/dto/UsernameDto.java
+++ Server/src/main/java/com/example/cryptoworld/models/dto/UsernameDto.java
@@ -1,4 +1,17 @@
 package com.example.cryptoworld.models.dto;
 
 public class UsernameDto {
+
+    private String username;
+
+    public UsernameDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
