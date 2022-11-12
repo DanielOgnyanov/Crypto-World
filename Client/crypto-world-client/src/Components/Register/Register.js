8cc5137155c6180ee4b2c14aacd8e13cada5a621
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-12 15:34:45 +0200

    on chanage input full name

--- Client/crypto-world-client/src/Components/Register/Register.js
+++ Client/crypto-world-client/src/Components/Register/Register.js
@@ -41,7 +41,7 @@ const Register = () => {
       
       
       <label htmlFor="fullname">Full Name</label>
-      <input name='fullname' placeholder='Full Name' type="text" id='text2'></input>
+      <input name='fullname' placeholder='Full Name' type="text" id='text2' onChange={fullNamehandler}></input>
       {fullNameError ? <span id='span-info-register'>Full name length must be between 5 and 20 characters.</span> : ""}
       
       <label htmlFor="email">Email</label>
