6ab57842d77a83925b95faef47942f73229af097
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-12 22:50:35 +0200

    fetch api to find if username is taken

--- Client/crypto-world-client/src/Services/UserInfoService.js
+++ Client/crypto-world-client/src/Services/UserInfoService.js
@@ -1,5 +1,12 @@
 const baseUrl = 'http://localhost:8000';
 
 export const findIfUsernameIsTakenInDb = async (username) => {
-    
+
+    let res = await fetch(`${baseUrl}/api/user/username/check`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ username })
+    });
 }
