95a31fdef4982050271680a7d54b0c187f0ab8dc
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-16 21:40:13 +0200

    span for password error

--- Client/crypto-world-client/src/Components/Register/Register.js
+++ Client/crypto-world-client/src/Components/Register/Register.js
@@ -152,7 +152,7 @@ const Register = () => {
 
       <label htmlFor="password">Password</label>
       <input name='password' placeholder='Password' type="password" id='text4' onChange={passwordHandler}></input>
-      {passError ? <span id='span-info-register' >Password lenght must be atleast 5 symbols</span> : ""}
+      {passError ? <span id='span-info-register' >Password lenght must be atleast 5 symbols and contains letters , numebrs and special symbols.</span> : ""}
 
       <label htmlFor="confirmPassword">Confirm Password</label>
       <input name='confirmPassword' placeholder='Confirm Password' type="password" id='text4'></input>
