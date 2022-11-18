007b61ea842951aaba406b9f0be74b47e8872195
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-18 19:51:32 +0200

    span is password correct green tick

--- Client/crypto-world-client/src/Components/Register/Register.js
+++ Client/crypto-world-client/src/Components/Register/Register.js
@@ -193,7 +193,7 @@ const Register = () => {
       <label htmlFor="password">Password</label>
       <input name='password' placeholder='Password' type="password" id='text4' onChange={passwordHandler}></input>
       {passError ? <span id='span-info-register' >Password lenght must be atleast 5 symbols and contains letters , numebrs and special symbols.</span> : ""}
-      {!passError  ? <span id='span-green-tick'>&#10004;</span> : ""}
+      {!passError && isPasswordCorrect ? <span id='span-green-tick'>&#10004;</span> : ""}
 
 
       <label htmlFor="confirmPassword">Confirm Password</label>
