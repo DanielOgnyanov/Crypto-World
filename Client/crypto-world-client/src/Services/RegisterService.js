8bef44cbe6abed489af02154cb3bb27c4d964253
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-20 20:44:59 +0200

    post request to register user

--- Client/crypto-world-client/src/Services/RegisterService.js
+++ Client/crypto-world-client/src/Services/RegisterService.js
@@ -2,4 +2,12 @@ const baseUrl = 'http://localhost:8000';
 
 export const register = async (username, fullname, email, password) => {
 
+    let res = await fetch(`${baseUrl}/api/user/register`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+
 }
