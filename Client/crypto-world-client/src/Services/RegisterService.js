15d965e651749c95e7f0fb816b9126711eaf4c70
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-24 22:06:20 +0200

    fetch change position of json input

--- Client/crypto-world-client/src/Services/RegisterService.js
+++ Client/crypto-world-client/src/Services/RegisterService.js
@@ -1,14 +1,16 @@
 const baseUrl = 'http://localhost:8000';
 
-export const register = async (username, fullName, email, password, country) => {
+export const register = async (username, fullName, email, country, password) => {
 
     let res = await fetch(`${baseUrl}/api/user/register`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({username, fullName, email, password, country})
+        body: JSON.stringify({username, fullName, email, country, password})
+        
     });
+    console.log(JSON.stringify({username, fullName, email, country, password}))
 
     let jsonResult = await res.json();
 
