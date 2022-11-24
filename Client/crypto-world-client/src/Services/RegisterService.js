06ce46d46fac7be1dc76a6e4c6d4227a1a9b7177
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-24 22:21:10 +0200

    add confirm password field to register service

--- Client/crypto-world-client/src/Services/RegisterService.js
+++ Client/crypto-world-client/src/Services/RegisterService.js
@@ -1,16 +1,16 @@
 const baseUrl = 'http://localhost:8000';
 
-export const register = async (username, fullName, email, country, password) => {
+export const register = async (username, fullName, email, country, password, confirmPassword) => {
 
     let res = await fetch(`${baseUrl}/api/user/register`, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
         },
-        body: JSON.stringify({username, fullName, email, country, password})
+        body: JSON.stringify({username, fullName, email, country, password, confirmPassword})
         
     });
-    console.log(JSON.stringify({username, fullName, email, country, password}))
+    console.log(JSON.stringify({username, fullName, email, country, password, confirmPassword}))
 
     let jsonResult = await res.json();
 
