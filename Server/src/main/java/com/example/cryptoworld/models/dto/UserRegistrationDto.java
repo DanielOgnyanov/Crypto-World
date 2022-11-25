d9ecfe1b80fefce75a941208595cd4353223ef99
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-26 00:16:37 +0200

    user register dto change fields

--- Server/src/main/java/com/example/cryptoworld/models/dto/UserRegistrationDto.java
+++ Server/src/main/java/com/example/cryptoworld/models/dto/UserRegistrationDto.java
@@ -18,10 +18,6 @@ public class UserRegistrationDto {
     private String confirmPassword;
 
 
-
-    public UserRegistrationDto() {
-    }
-
     public String getUsername() {
         return username;
     }
