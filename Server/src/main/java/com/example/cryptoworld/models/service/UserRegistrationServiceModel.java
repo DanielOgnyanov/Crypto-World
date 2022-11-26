9dd72a12699c8c514828fecd5be8befa1a428c63
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-11-26 18:27:19 +0200

    adding constructor

--- Server/src/main/java/com/example/cryptoworld/models/service/UserRegistrationServiceModel.java
+++ Server/src/main/java/com/example/cryptoworld/models/service/UserRegistrationServiceModel.java
@@ -12,18 +12,7 @@ public class UserRegistrationServiceModel {
     private String password;
     private String confirmPassword;
 
-    public UserRegistrationServiceModel(String username,
-                                        String fullName,
-                                        String email,
-                                        EnumCountry country,
-                                        String password,
-                                        String confirmPassword) {
-        this.username = username;
-        this.fullName = fullName;
-        this.email = email;
-        this.country = country;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
+    public UserRegistrationServiceModel() {
     }
 
     public String getUsername() {
