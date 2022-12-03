ca05a50e70c0a7b9af3147adcffc470dd0e2a66b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-03 17:13:37 +0200

    adding trigger in popup

--- Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
+++ Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
@@ -4,25 +4,20 @@ import './PopupRegister.css'
 export const PopupRegister = (e) => {
 
 
-    return(
+    return (e.trigger) ? (
         <div id='popup'>
 
             <div id='popup-inner'>
 
-                <div id='check-mark-div'> 
-                <span id='span-tick'>&#10004;</span>
-                <h3 id='h3-success'>Success</h3>
+                <div id='check-mark-div'>
+                    <span id='span-tick'>&#10004;</span>
+                    <h3 id='h3-success'>Success</h3>
                 </div>
 
                 <h4 id='h4-info-text'>Congratulations, your account has been successfully created.</h4>
 
                 <button id='continue-button'>Continue</button>
-
-            
-
             </div>
-            
-
         </div>
-    )
+    ) : ""
 };
