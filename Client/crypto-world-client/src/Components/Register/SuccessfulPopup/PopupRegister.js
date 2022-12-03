3ab04b99703dabb9bfcd8621aec07cb8484122c2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-03 17:41:28 +0200

    adding class name

--- Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
+++ Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
@@ -5,7 +5,7 @@ export const PopupRegister = (e) => {
 
 
     return (e.trigger) ? (
-        <div id='popup'>
+        <div className='popup' id='popup'>
 
             <div className='popup-inner' id='popup-inner'>
 
