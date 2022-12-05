bac00540a8fd62aab562cfb536f2adca0db38907
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-05 22:01:36 +0200

    change trigger

--- Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
+++ Client/crypto-world-client/src/Components/Register/SuccessfulPopup/PopupRegister.js
@@ -1,10 +1,10 @@
 import Popup from 'reactjs-popup';
 import './PopupRegister.css'
 
-export const PopupRegister = (e) => {
+export const PopupRegister = (props) => {
 
 
-    return (e) ?(
+    return (props.trigger) ? (
         <div className='popup' id='popup'>
 
             <div className='popup-inner' id='popup-inner'>
@@ -16,7 +16,7 @@ export const PopupRegister = (e) => {
 
                 <h4 className='h4-info-text' id='h4-info-text'>Congratulations, your account has been successfully created.</h4>
 
-                <button classNamem='continue-button' id='continue-button' onClick={() => e.setTrigger(false)}>Continue</button>
+                <button classNamem='continue-button' id='continue-button' onClick={() => props.setTrigger(false)}>Continue</button>
             </div>
         </div>
     ) : ""
