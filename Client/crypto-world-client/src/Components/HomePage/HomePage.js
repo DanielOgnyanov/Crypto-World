cac4971e572a36df2f578fe19c715b503533abd0
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-07 21:52:09 +0200

    import redirect

--- Client/crypto-world-client/src/Components/HomePage/HomePage.js
+++ Client/crypto-world-client/src/Components/HomePage/HomePage.js
@@ -2,7 +2,7 @@ import './HomePage.css'
 import * as cryptoService from '../../Services/CryptoService'
 import Chart from '../Chart/Chart';
 import WalletDetails  from '../WalletDetails/WalletDetails';
-import Register, {redirect} from '../Register/Register';
+import {redirect} from '../Register/Register';
 import  {PopupRegister}  from '../Register/SuccessfulPopup/PopupRegister';
 
 
