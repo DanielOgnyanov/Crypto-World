b8ac61732c606f5102c6e23e258def7b8d7980f9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-16 19:00:16 +0200

    auth service fetch

--- Client/crypto-world-client/src/Services/authService.js
+++ Client/crypto-world-client/src/Services/authService.js
@@ -1,7 +1,7 @@
 const baseUrl = 'http://localhost:8000';
 
 export const login = async (username, password) => {
-    
+
     let res = fetch(`${baseUrl}/api/user/login`, {
         method: 'POST',
         headers: {
@@ -10,21 +10,27 @@ export const login = async (username, password) => {
         body: JSON.stringify({ username, password })
     });
 
-    
-    res.then((response) =>{
-        
+
+    res.then((response) => {
+
         if (!response.ok) {
             throw new response.status;
         }
-    })
+        
+        localStorage.setItem("username" ,JSON.stringify (username));
 
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+    });
 
-export const getUser = () => {
-    let username = localStorage.getItem('username');
+}
+    export const getUser = () => {
+        let username = localStorage.getItem('username');
 
-    return username;
-};
+        return username;
+    };
 
-export const isAuthenticated = () => {
-    return Boolean(getUser())
-};
+    export const isAuthenticated = () => {
+        return Boolean(getUser())
+    };
