09b08efa53a5927e664a33d0baccb3ab1c08e39a
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-16 17:28:43 +0200

    input error message

--- Client/crypto-world-client/src/Components/Login/Login.js
+++ Client/crypto-world-client/src/Components/Login/Login.js
@@ -87,7 +87,7 @@ const Login = () => {
 
             <label htmlFor="username" id='login-label'>Username</label>
             <input name='username' autoComplete="on" placeholder='Username' type="text" id='login-username-input' onChange={userHandler} />
-            
+            {inputError ? <span id='span-info' >Input cannot be empty</span> : ""}
 
             <label htmlFor="password" id='login-label'>Password</label>
             <input name='password' autoComplete="on" placeholder='Password' type="password" id='login-password-input' onChange={passwordHandler}/>
