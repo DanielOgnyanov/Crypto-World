ca323e3fbe0f2f8d123d0d36009da96ad5ef07e9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-17 22:15:12 +0200

    import id

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
     return (
 
         <header>
-            <nav className="topnav">
+            <nav className="topnav" >
                 <Link className='button' to="/" ><img src={logoImg} alt=""></img></Link>
 
                 <div className='userNavigation'>
