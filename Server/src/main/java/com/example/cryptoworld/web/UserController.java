5eabc96821d591668fde274eebe6afa25f8d21e4
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-19 21:59:21 +0200

    add cross origin

--- Server/src/main/java/com/example/cryptoworld/web/UserController.java
+++ Server/src/main/java/com/example/cryptoworld/web/UserController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/user")
 public class UserController {
 
