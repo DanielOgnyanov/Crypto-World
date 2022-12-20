60cde76751aa31c1fef569de870e12bf9a6ca5a6
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-20 21:48:08 +0200

    history push home

--- Client/crypto-world-client/src/Components/Login/Login.js
+++ Client/crypto-world-client/src/Components/Login/Login.js
@@ -71,6 +71,8 @@ const Login = () => {
 
             if(usernameGet === null){
                 setWrongCredentialsError(true);
+            } else {
+                history("/home")
             }
 
 
