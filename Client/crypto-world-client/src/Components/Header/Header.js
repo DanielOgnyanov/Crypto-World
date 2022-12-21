dc7e48d666fb08b2f8890f2f21a412d61ba38f0c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-21 22:39:10 +0200

    guest or user header options

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -50,6 +50,14 @@ const Header = () => {
         <header>
             <nav id='navigation'>
 
+                {user.username 
+
+                ? guestNavigation
+                : userNavigation
+
+                }
+
+
 
                 
             </nav>
