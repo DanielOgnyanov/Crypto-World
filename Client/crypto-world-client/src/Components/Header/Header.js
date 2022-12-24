6fbeb0405287f08848783eb62d5a2bc4900d9a29
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-24 17:53:59 +0200

    navigaion-a-div

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -23,12 +23,14 @@ const Header = () => {
 
             <Link to="/"><span id='logo-header'>Crypto World</span></Link>
 
+            <div id='navigaion-a-div'>
             <Link id='navigation-a' to="/home" data-toggle="collapse">Home</Link>
             <Link id='navigation-a' to="/admin" data-toggle="collapse">Admin</Link>
 
             <Link id='navigation-a' to="/add-credit-card">Add Credit Card</Link>
             <Link id='navigation-a' to="/buy-crypto">Buy Crypto</Link>
             <Link id='navigation-a' to="/sell-crypto">Sell Crypto</Link>
+            </div>
 
             <button id='logout-button'>Logout</button>
 
@@ -61,12 +63,12 @@ const Header = () => {
 
 
 
-                {isAuthenticated
+               
 
-                    ? userNavigation 
-                    : guestNavigation
+                     {userNavigation} 
+                     
 
-                }
+                
 
 
 
