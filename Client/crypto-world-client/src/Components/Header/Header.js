c8a31e8d10aa9fe9f5c61989efb5d36b7a1c8a32
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-25 22:37:02 +0200

    submit button

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -39,7 +39,7 @@ const Header = () => {
             </div>
 
 
-            <button onClick={logout} id='logout-button'>Logout</button>
+            <button onClick={logout} type='submit' id='logout-button'>Logout</button>
 
         </div>
 
