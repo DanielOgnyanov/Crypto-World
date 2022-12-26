afe43d87e42aeb5a8b21452a7aeb902f26798d11
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-26 15:27:02 +0200

    div init for index page

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -2,6 +2,25 @@ import './IndexPage.css'
 
 const IndexPage = () => {
 
+
+    return(
+
+        <div className='index-page'>
+
+            <div className='get-started'>
+
+            </div>
+
+
+
+        </div>
+
+
+
+
+
+    );
+
 }
 
 export default IndexPage;
