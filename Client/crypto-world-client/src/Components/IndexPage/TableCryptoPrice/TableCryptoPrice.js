cd12f4d04ac6f013e655221a2e4d973d30fd4634
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-28 21:09:14 +0200

    td in table add

--- Client/crypto-world-client/src/Components/IndexPage/TableCryptoPrice/TableCryptoPrice.js
+++ Client/crypto-world-client/src/Components/IndexPage/TableCryptoPrice/TableCryptoPrice.js
@@ -7,21 +7,26 @@ const TableCryptoPrice = () => {
     return (
 
         <div id='container-crypto-price-table'>
-            
+
             <div id='table-price-div '>
-            <table id='table-price'>
-                <th>#</th>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Market Cap</th>
+                <table id='table-price'>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>Market Cap</th>
+
+                    <tr>
+                        <td>Bitcoin</td>
+                        
+                    </tr>
 
 
 
-            </table>
+                </table>
             </div>
-            
-            
-            
+
+
+
 
 
 
@@ -33,7 +38,7 @@ const TableCryptoPrice = () => {
 
 
 
-        
+
     );
 
 }
