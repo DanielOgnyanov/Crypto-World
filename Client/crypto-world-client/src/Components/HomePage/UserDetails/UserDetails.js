fbd57bd22e26803058894763bb1b4bfaac7c7664
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-29 18:31:45 +0200

    currentUsername get from localstorage

--- Client/crypto-world-client/src/Components/HomePage/UserDetails/UserDetails.js
+++ Client/crypto-world-client/src/Components/HomePage/UserDetails/UserDetails.js
@@ -4,6 +4,9 @@ import './UserDetails.css'
 const UserDetails = () => {
 
 
+    const currentUsername = JSON.parse(localStorage.getItem("usernameLogin"))
+
+
     return(
         <div className='user-info' id='user-details-welcome'>
 
