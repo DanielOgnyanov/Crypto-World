e49dc978b3871a9bdb169fa6e45dee1ffecb272f
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-30 19:18:14 +0200

    method: 'POST'

--- Client/crypto-world-client/src/Services/authService.js
+++ Client/crypto-world-client/src/Services/authService.js
@@ -38,6 +38,7 @@ export const login = async (username, password) => {
 export const logout = async (username) => {
 
     let res = fetch(`${baseUrl}/api/user/logout`, {
+        method: 'POST'
 
     });
 
