6603493ea2650a71bd2c54d01486b4d1f7a9002d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2022-12-30 19:21:21 +0200

    remove item

--- Client/crypto-world-client/src/Components/Header/Header.js
+++ Client/crypto-world-client/src/Components/Header/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
 
     function logout() {
         
-        localStorage.removeItem("usernameLogin");
+        
         setIsAuthenticated(false);
         history('/')
     }
