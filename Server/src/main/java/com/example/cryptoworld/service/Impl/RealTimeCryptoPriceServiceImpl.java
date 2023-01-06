72e21155f34db7172212063607c984a8befcf481
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-01-06 17:44:51 +0200

    dotcoin logo set

--- Server/src/main/java/com/example/cryptoworld/service/Impl/RealTimeCryptoPriceServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/RealTimeCryptoPriceServiceImpl.java
@@ -114,6 +114,8 @@ public class RealTimeCryptoPriceServiceImpl implements RealTimeCryptoPriceServic
                 break;
             case "DogeCoin":
                 imagePath = "src\\main\\resources\\static\\images\\dogecoin-logo.png";
+                break;
+            case "Dotcoin":
 
 
 
