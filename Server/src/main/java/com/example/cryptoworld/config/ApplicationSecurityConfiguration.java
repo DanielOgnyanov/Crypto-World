d5434c54e03f1acc76cfa8af9443e49bcc6d1e59
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-01-10 19:34:38 +0200

    addCorsMapping method

--- Server/src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
+++ Server/src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebSecurity
@@ -65,6 +66,12 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Override
+    public void addCorsMapping(CorsRegistry registry) {
+
+        registry.addMapping()
     }
 
 
