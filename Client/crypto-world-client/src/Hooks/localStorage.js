1a0e74beda614d3a6af9d424dfd2efdb06de8fe7
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-02-24 21:19:57 +0200

    useLocalStorage initial set

--- Client/crypto-world-client/src/Hooks/localStorage.js
+++ Client/crypto-world-client/src/Hooks/localStorage.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
     const [state, setState] = useState(() => {
+        
         try {
             let item = localStorage.getItem(key);
 
