ccd15c77463b39def70fa58b388aed7ce2aa3315
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-03-24 21:17:34 +0200

    username-top-five-crypto input css change

--- Client/crypto-world-client/src/Components/AdminPanel/TopFiveCryptoBuy/TopFiveCryptoBuy.js
+++ Client/crypto-world-client/src/Components/AdminPanel/TopFiveCryptoBuy/TopFiveCryptoBuy.js
@@ -23,14 +23,14 @@ const TopFiveCryptoBuy = () => {
         <form id='form-top-five-crypto-buy'>
 
 
+            <p id='p-top-five-crypto-by-user'>Top 5 Crypto Buy By User</p>
 
-            <label htmlFor="Username" >Username</label>
+            <label htmlFor="Username" id='label-top-five-crypto-by-user'>Username</label>
             <input name='username' autoComplete="on" placeholder='Username' type="text" id='username-top-five-crypto' />
 
-            <button className="button" type="submit" >Change Role</button>
+            
 
-
-            <p>Top 5 Crypto Buy By User</p>
+            <button className="button" type="submit" id='button-find-user-last-five-buy'>Search</button>
 
 
 
