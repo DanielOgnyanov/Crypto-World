2e5f8d5952e54374e12dd27374b9b60d1136071c
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-03-28 21:43:06 +0300

    section init

--- Client/crypto-world-client/src/Components/AdminPanel/AdminPanel.js
+++ Client/crypto-world-client/src/Components/AdminPanel/AdminPanel.js
@@ -13,29 +13,26 @@ const AdminPanel = () => {
 
     return (
 
-        <div className='container-admin-panel' id='container-admin-panel'>
+        <>
+            <div className='container-admin-panel' id='container-admin-panel'>
+
+                <section className='change-user-role' id='change-user-role'>
+                    <ChangeUserRole />
+
+                </section>
+                
+                <section className='user-top-five-buy' id='user-top-five-buy'>
+                    <TopFiveCryptoBuy />
+                </section>
+
+            </div>
 
 
-            <section className='user-top-five-buy' id='user-top-five-buy'>
-                <TopFiveCryptoBuy />
-            </section>
-
-            <section className='change-user-role' id='change-user-role'>
-                <ChangeUserRole />
-
-            </section>
-
-            <section className='logged-users' id='logged-users'>
+            <div className='logged-users' id='logged-users'>
                 <LoggedUsers />
-            </section>
+            </div>
 
-
-
-
-        </div>
-
-
-
+        </>
 
 
     );
