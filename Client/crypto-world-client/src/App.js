9a83192002e56728e767209dbd6c7a2f972c4ca8
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-04-07 19:08:54 +0300

    logout

--- Client/crypto-world-client/src/App.js
+++ Client/crypto-world-client/src/App.js
@@ -35,7 +35,7 @@ function App() {
           <Route path = "/buy-crypto" element = {<BuyCrypto/>}/>
           <Route path = "/sell-crypto" element = {<SellCrypto/>}/>
           <Route path = "/admin" element = {<AdminPanel/>}/>
-          <Route path="/" element={<Logout />} />
+          <Route path="/logout" element={<Logout/>} />
 
         </Routes>
 
