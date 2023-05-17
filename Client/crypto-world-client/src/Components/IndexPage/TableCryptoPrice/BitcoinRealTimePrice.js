225a4a47827b3ae6b9f748737f744e2a4ee479de
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-17 20:16:51 +0300

    add id css

--- Client/crypto-world-client/src/Components/IndexPage/TableCryptoPrice/BitcoinRealTimePrice.js
+++ Client/crypto-world-client/src/Components/IndexPage/TableCryptoPrice/BitcoinRealTimePrice.js
@@ -7,26 +7,11 @@ const BitcoinRealTimePrice = () => {
 
     return (
 
-        <div id='container-crypto-price-table'>
+       <div id='container-crypto-price-table'>
 
-            <div id='table-price-div '>
-                <table id='table-price'>
-                    <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Price</th>
-                        <th>Market Cap</th>
-                    </tr>
-                    <tr>
-                        <td>1</td>
-                        <td>Bitcoin</td>
-                        <td>16 000</td>
-                        <td>$319,855,199,176</td>
+        
 
-                    </tr>
-                </table>
-            </div>
-        </div>
+       </div>
 
 );
 
