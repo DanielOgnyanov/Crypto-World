a64815c3647f5b24fedc9ba530db4e2c48ce1041
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-19 20:38:54 +0300

    Ethereum Real Time Price

---
+++ Client/crypto-world-client/src/Components/IndexPage/PopularCrypto/Ethreum/EthereumRealTimePrice.js
@@ -0,0 +1,28 @@
+import './EthereumRealTimePrice.css'
+
+
+
+const EthereumRealTimePrice = () => {
+
+
+
+    return (
+
+       <div id='container-crypto-price-table'>
+
+        <img id='bitcoin-logo' src = {BitcoinImg}></img>
+
+        <p id='item-name-position'>Bitcoin</p>
+
+        <p id='real-time-price'>28.000.00 $</p>
+
+        <button id='buy-button'>Buy</button>
+
+       </div>
+
+);
+
+}
+
+
+export default EthereumRealTimePrice;
