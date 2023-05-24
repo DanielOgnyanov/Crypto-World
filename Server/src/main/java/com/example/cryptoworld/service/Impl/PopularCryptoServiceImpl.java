159166e11267f71b4d6d2d52eafbc20c220d8346
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-24 17:15:19 +0300

    popular crypto service impl

--- Server/src/main/java/com/example/cryptoworld/service/Impl/PopularCryptoServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/PopularCryptoServiceImpl.java
@@ -1,4 +1,17 @@
 package com.example.cryptoworld.service.Impl;
 
-public class PopularCryptoServiceImpl {
+import com.example.cryptoworld.repository.CryptoRepository;
+import com.example.cryptoworld.service.PopularCryptoService;
+import org.modelmapper.ModelMapper;
+
+public class PopularCryptoServiceImpl implements PopularCryptoService {
+
+    private final CryptoRepository cryptoRepository;
+    private final ModelMapper modelMapper;
+
+
+    public PopularCryptoServiceImpl(CryptoRepository cryptoRepository, ModelMapper modelMapper) {
+        this.cryptoRepository = cryptoRepository;
+        this.modelMapper = modelMapper;
+    }
 }
