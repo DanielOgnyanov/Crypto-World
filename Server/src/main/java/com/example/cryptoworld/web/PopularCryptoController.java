4e37fcde1823910879c5a567b2a1855bbfbd2b72
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-24 17:38:36 +0300

    popular crypto controller

--- Server/src/main/java/com/example/cryptoworld/web/PopularCryptoController.java
+++ Server/src/main/java/com/example/cryptoworld/web/PopularCryptoController.java
@@ -1,4 +1,12 @@
 package com.example.cryptoworld.web;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/index/popular/crypto")
 public class PopularCryptoController {
 }
