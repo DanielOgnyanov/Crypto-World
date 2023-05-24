0fa30199459b570af07df8483586b5018f8baa4d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-24 17:20:55 +0300

    popular crypto service impl

--- Server/src/main/java/com/example/cryptoworld/service/PopularCryptoService.java
+++ Server/src/main/java/com/example/cryptoworld/service/PopularCryptoService.java
@@ -1,4 +1,10 @@
 package com.example.cryptoworld.service;
 
+import com.example.cryptoworld.models.view.PopularCryptoViewModel;
+
+import java.util.List;
+
 public interface PopularCryptoService {
+
+    List<PopularCryptoViewModel> getPopularCrypto();
 }
