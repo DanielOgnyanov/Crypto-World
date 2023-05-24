75ab4044ae8c71ca87c783388ae72593ca4f7427
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-24 17:47:43 +0300

    popular crypto controller

--- Server/src/main/java/com/example/cryptoworld/web/PopularCryptoController.java
+++ Server/src/main/java/com/example/cryptoworld/web/PopularCryptoController.java
@@ -1,14 +1,19 @@
 package com.example.cryptoworld.web;
 
 
+import com.example.cryptoworld.models.view.PopularCryptoViewModel;
 import com.example.cryptoworld.service.PopularCryptoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/api/index/popular/crypto")
+@RequestMapping("/api/index")
 public class PopularCryptoController {
 
 
@@ -17,5 +22,13 @@ public class PopularCryptoController {
 
     public PopularCryptoController(PopularCryptoService popularCryptoService) {
         this.popularCryptoService = popularCryptoService;
+    }
+
+
+    @GetMapping("/popular/crypto")
+    public ResponseEntity<List<PopularCryptoViewModel>> getPopularCrypto() {
+
+
+        return
     }
 }
