f4a672beeda710ae1175ba8fa1d73f744611f287
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-27 20:05:44 +0300

    fetch data from api

--- Client/crypto-world-client/src/Components/IndexPage/PopularCrypto/Bitcoin/BitcoinRealTimePrice.js
+++ Client/crypto-world-client/src/Components/IndexPage/PopularCrypto/Bitcoin/BitcoinRealTimePrice.js
@@ -8,55 +8,54 @@ import { useState, useEffect } from 'react';
 
 const BitcoinRealTimePrice = () => {
 
-    const [data, setData] = useState([]);
-    const [filteredPrice, setFilteredPrice] = useState('');
+  const [data, setData] = useState([]);
+  const [filteredPrice, setFilteredPrice] = useState('');
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const fetchResult = await getPopularCryptoPrice();
-          setData(fetchResult);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      
-      fetchData();
-  
-      
-      const interval = setInterval(fetchData, 55000); // Todo: set time for updating the price
-  
-      
-      return () => clearInterval(interval);
-    }, []);
-    
-      
-      useEffect(() => {
-        
-        const filteredData = data.filter(item => item.name === 'Bitcoin');
-    
-        if (filteredData.length > 0) {
-          const price = filteredData[0].price.toFixed(2);
-          setFilteredPrice(price);
-        }
-      }, [data]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetchResult = await getPopularCryptoPrice();
+        setData(fetchResult);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    return (
 
-       <div id='container-crypto-price-table'>
+    fetchData();
 
-        <img id='logo-position' src = {BitcoinImg}></img>
+    const interval = setInterval(fetchData, 55000); // Todo: set time for updating the price
 
-        <p id='item-name-position'>Bitcoin</p>
 
-        <p id='real-time-price'>{filteredPrice} $</p>
+    return () => clearInterval(interval);
+  }, []);
 
-        <button id='buy-button'>Buy</button>
 
-       </div>
+  useEffect(() => {
 
-);
+    const filteredData = data.filter(item => item.name === 'Bitcoin');
+
+    if (filteredData.length > 0) {
+      const price = filteredData[0].price.toFixed(2);
+      setFilteredPrice(price);
+    }
+  }, [data]);
+
+  return (
+
+    <div id='container-crypto-price-table'>
+
+      <img id='logo-position' src={BitcoinImg}></img>
+
+      <p id='item-name-position'>Bitcoin</p>
+
+      <p id='real-time-price'>{filteredPrice} $</p>
+
+      <button id='buy-button'>Buy</button>
+
+    </div>
+
+  );
 
 }
 
