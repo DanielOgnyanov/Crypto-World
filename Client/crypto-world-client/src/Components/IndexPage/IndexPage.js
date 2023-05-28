4b2d1cc339c2fbe32b94900e4fb5d45225a46472
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-28 21:52:34 +0300

    add color text

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -19,7 +19,7 @@ const IndexPage = () => {
 
             <section id='crypto-information'>
 
-            <p><span id='bold-text'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information. 
+            <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information. 
             Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio. 
             Join the crypto community, access advanced tools, and embark on an exciting trading journey. 
             Start today and unleash the potential of digital assets!</p>
