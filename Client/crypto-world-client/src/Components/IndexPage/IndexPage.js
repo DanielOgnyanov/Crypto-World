d2fda4202f674270cb1de8b2263e372a252a4a0d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-28 21:51:23 +0300

    add section

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -17,6 +17,15 @@ const IndexPage = () => {
                 <GetStarted />
             </div>
 
+            <section id='crypto-information'>
+
+            <p><span id='bold-text'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information. 
+            Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio. 
+            Join the crypto community, access advanced tools, and embark on an exciting trading journey. 
+            Start today and unleash the potential of digital assets!</p>
+
+            </section>
+
             <p id='p-popular-crypto'>Popular Crypto</p>
 
             <div id='container-popular-crypto'>
