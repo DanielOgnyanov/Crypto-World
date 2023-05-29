a3f3e14f3feb6da20bb2fbc1d2c673a369930f33
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-29 19:58:11 +0300

    div container

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -5,7 +5,7 @@ import EthereumRealTimePrice from './PopularCrypto/Ethreum/EthereumRealTimePrice
 import BnbRealTimePrice from './PopularCrypto/BNB/BnbRealTimePrice';
 import TetherRealTimePrice from './PopularCrypto/Tether/TetherRealTimePrice';
 import { useAuthContext } from '../../Context/AuthContext';
-import {cryptoGirlImg} from '../../Images/crypto-img-index-page.jpg'
+import cryptoGirlImg from '../../Images/crypto-img-index-page.jpg'
 
 const IndexPage = () => {
 
@@ -18,20 +18,28 @@ const IndexPage = () => {
                 <GetStarted />
             </div>
 
-            <section>
-             
-             <img></img>
 
-            </section>
+            <div>
 
-            <section id='crypto-information'>
+                <div id='crypto-information'>
 
-                <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information.
-                    Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio.
-                    Join the crypto community, access advanced tools, and embark on an exciting trading journey.
-                    Start today and unleash the potential of digital assets!</p>
+                    <img id='crypto-girl-img' src={cryptoGirlImg}></img>
 
-            </section>
+                    <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information.
+                        Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio.
+                        Join the crypto community, access advanced tools, and embark on an exciting trading journey.
+                        Start today and unleash the potential of digital assets!</p>
+
+                </div>
+
+
+
+            </div>
+
+
+
+
+
 
             <p id='p-popular-crypto'>Popular Crypto</p>
 
