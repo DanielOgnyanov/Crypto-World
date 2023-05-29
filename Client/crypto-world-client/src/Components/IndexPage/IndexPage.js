0e7b64bf9aba6a1993e33fadd841af5cc3dcd301
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-29 19:48:58 +0300

    import img

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -5,6 +5,7 @@ import EthereumRealTimePrice from './PopularCrypto/Ethreum/EthereumRealTimePrice
 import BnbRealTimePrice from './PopularCrypto/BNB/BnbRealTimePrice';
 import TetherRealTimePrice from './PopularCrypto/Tether/TetherRealTimePrice';
 import { useAuthContext } from '../../Context/AuthContext';
+import {cryptoGirlImg} from '../../Images/crypto-img-index-page.jpg'
 
 const IndexPage = () => {
 
@@ -17,12 +18,18 @@ const IndexPage = () => {
                 <GetStarted />
             </div>
 
+            <section>
+             
+             <img></img>
+
+            </section>
+
             <section id='crypto-information'>
 
-            <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information. 
-            Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio. 
-            Join the crypto community, access advanced tools, and embark on an exciting trading journey. 
-            Start today and unleash the potential of digital assets!</p>
+                <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information.
+                    Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio.
+                    Join the crypto community, access advanced tools, and embark on an exciting trading journey.
+                    Start today and unleash the potential of digital assets!</p>
 
             </section>
 
@@ -32,7 +39,7 @@ const IndexPage = () => {
 
                 <BitcoinRealTimePrice />
                 <EthereumRealTimePrice />
-                
+
             </div>
 
             <div id='container-popular-crypto'>
