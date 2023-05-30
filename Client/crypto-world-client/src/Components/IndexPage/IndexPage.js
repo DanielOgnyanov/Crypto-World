8c509297d6520574a8ae14b52718fe6f9a484e5b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-30 20:17:13 +0300

    add span info

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -76,10 +76,10 @@ const IndexPage = () => {
 
 
                 <div id='crypto-information'>
-                <p id="hold-earn-text">Discover the potential of "<span id='bold-text-hold-and-earn'>hold and earn</span>" crypto with our platform.
+                <p id="hold-earn-text">Discover the potential of <span id='bold-text-hold-and-earn'>Hold and Earn</span> crypto with our platform.
                 By holding onto your chosen cryptocurrencies, you can unlock a variety of earning opportunities.
                 Stake your assets in proof-of-stake networks, lend them to borrowers for interest, or participate in yield farming on decentralized platforms.
-                Take advantage of the power of compounding and watch your crypto portfolio grow.
+                Take <span id='bold-text-hold-and-earn'>advantage</span> of the power of compounding and watch your crypto portfolio grow.
                 Whether you're a long-term investor or looking for additional income streams, our platform offers a range of options to suit your needs.
                 Start earning passive income with crypto today and make your assets work for you.</p>
 
