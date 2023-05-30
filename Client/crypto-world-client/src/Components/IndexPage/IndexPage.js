d44d7263700f60c18de3a772ffc6eb5932393432
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-30 20:13:12 +0300

    add hold and earn description with css

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -75,6 +75,17 @@ const IndexPage = () => {
                 </div>
 
 
+                <div id='crypto-information'>
+                <p id="hold-earn-text">Discover the potential of "<strong>hold and earn</strong>" crypto with our platform.
+                By holding onto your chosen cryptocurrencies, you can unlock a variety of earning opportunities.
+                Stake your assets in proof-of-stake networks, lend them to borrowers for interest, or participate in yield farming on decentralized platforms.
+                Take advantage of the power of compounding and watch your crypto portfolio grow.
+                Whether you're a long-term investor or looking for additional income streams, our platform offers a range of options to suit your needs.
+                Start earning passive income with crypto today and make your assets work for you.</p>
+
+                </div>
+
+
 
             </div>
 
