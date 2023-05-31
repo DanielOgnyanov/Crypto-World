64ecdd49feea278cc9d93a4c6cffbba2bfe1e355
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-31 20:52:16 +0300

    span-info

--- Client/crypto-world-client/src/Components/IndexPage/BuildYourPortfolioInfo/BuildYourPortfolioInfo.js
+++ Client/crypto-world-client/src/Components/IndexPage/BuildYourPortfolioInfo/BuildYourPortfolioInfo.js
@@ -11,6 +11,8 @@ const BuildYourPortfolioInfo = () => {
      
      <p id='p-build-your-crypto-portfolio'>Build your crypto portfolio</p>
      
+     <span>Start your first trade with these easy steps.</span>
+     
      
      
      
