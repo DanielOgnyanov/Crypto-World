ee418cd52206a6398a091a7d60c72bc01153bf2d
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-31 20:26:18 +0300

    hold and earn init

---
+++ Client/crypto-world-client/src/Components/IndexPage/HoldAndEarnInfo/HoldAndEarn.js
@@ -0,0 +1,48 @@
+const HoldAndEarn = () => {
+
+
+    return (
+
+        <>
+
+            <p id='p-hold-earn'>Hold and Earn</p>
+
+
+            <div id='information-div'>
+
+                <div className='hold-and-earn-img'>
+
+                    <img id='hold-and-earn-img' src={holdEarnImg}></img>
+
+                </div>
+
+
+                <div id='crypto-information'>
+                    <p id="hold-earn-text">Discover the potential of <span id='bold-text-hold-and-earn'>Hold and Earn</span> crypto with our platform.
+                        By holding onto your chosen cryptocurrencies, you can unlock a variety of earning opportunities.
+                        Stake your assets in proof-of-stake networks, lend them to borrowers for interest, or participate in yield farming on decentralized platforms.
+                        Take <span id='bold-text-hold-and-earn'>advantage</span> of the power of compounding and watch your crypto portfolio grow.
+                        Whether you're a long-term investor or looking for additional income streams, our platform offers a range of options to suit your needs.
+                        Start earning passive income with crypto today and make your assets work for you.</p>
+
+                </div>
+
+
+
+            </div>
+
+
+
+
+
+
+        </>
+
+
+
+
+    );
+
+}
+
+export default HoldAndEarn;
