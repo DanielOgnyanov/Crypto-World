e04ba01069e5627a85dd99fb0e55111de2e82abe
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-31 20:18:24 +0300

    welcome init

---
+++ Client/crypto-world-client/src/Components/IndexPage/WelcomeInfo/Welcome.js
@@ -0,0 +1,41 @@
+const Welcome = () => {
+
+
+    return (
+
+
+
+        <>
+
+
+            <div id='crypto-girl-div'>
+
+                <img id='crypto-girl-img' src={cryptoGirlImg}></img>
+
+            </div>
+
+            <div id='crypto-information'>
+
+
+
+                <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information.
+                    Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio.
+                    Join the crypto community, access advanced tools, and embark on an exciting trading journey.
+                    Start today and unleash the potential of digital assets!</p>
+
+            </div>
+
+
+
+
+
+        </>
+
+
+
+
+    );
+
+}
+
+export default Welcome;
