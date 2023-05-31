6c43c4394ef3b95908674c9a087f5797be907822
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-05-31 20:22:12 +0300

    import component into index page

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -5,8 +5,8 @@ import EthereumRealTimePrice from './PopularCrypto/Ethreum/EthereumRealTimePrice
 import BnbRealTimePrice from './PopularCrypto/BNB/BnbRealTimePrice';
 import TetherRealTimePrice from './PopularCrypto/Tether/TetherRealTimePrice';
 import { useAuthContext } from '../../Context/AuthContext';
-import cryptoGirlImg from '../../Images/crypto-girl-image.PNG'
 import holdEarnImg from '../../Images/hold-and-earn-img.PNG'
+import Welcome from './WelcomeInfo/Welcome';
 
 const IndexPage = () => {
 
@@ -21,27 +21,7 @@ const IndexPage = () => {
 
 
             <div id='information-div'>
-
-
-                <div id='crypto-girl-div'>
-
-                    <img id='crypto-girl-img' src={cryptoGirlImg}></img>
-
-                </div>
-
-                <div id='crypto-information'>
-
-
-
-                    <p><span id='bold-text-welcome'>Welcome to Crypto World</span>, <br></br> your go-to destination for <span id='bold-text'>crypto trading</span> and market information.
-                        Stay updated with real-time data, charts, and analysis. Discover a wide range of <span id='bold-text'>cryptocurrencies</span>, explore trading pairs, and diversify your portfolio.
-                        Join the crypto community, access advanced tools, and embark on an exciting trading journey.
-                        Start today and unleash the potential of digital assets!</p>
-
-                </div>
-
-
-
+                <Welcome />
             </div>
 
 
