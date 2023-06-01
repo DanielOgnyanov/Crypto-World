13b82de2e7be7cb357804f4be058ade11ac54bd9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-01 20:25:21 +0300

    build portfolio ok

--- Client/crypto-world-client/src/Components/IndexPage/BuildYourPortfolioInfo/BuildYourPortfolioInfo.js
+++ Client/crypto-world-client/src/Components/IndexPage/BuildYourPortfolioInfo/BuildYourPortfolioInfo.js
@@ -1,6 +1,7 @@
 import './BuildYourPortfolioInfo.css'
 import verifyImg from '../../../Images/verify-img.jpg'
 import fundImg from '../../../Images/fund-img.jpg'
+import tradingImg from '../../../Images/trading-img.jpg'
 
 
 const BuildYourPortfolioInfo = () => {
@@ -26,12 +27,19 @@ const BuildYourPortfolioInfo = () => {
                 <img id='img-step' src={fundImg}></img>
 
                 <p id='p-step-title'>Fund your account</p>
-               
+
             </div>
             <p id='p-text'>Add funds to your crypto account to start trading crypto. You can add funds with a variety of payment methods.</p>
 
 
+            <div id='info-display-block-inline'>
+                <img id='img-step' src={tradingImg}></img>
 
+                <p id='p-step-title'>Start trading</p>
+
+            </div>
+            <p id='p-text'>You're good to go! Buy/sell crypto, set up recurring buys for your investments.
+                <br></br> Discover what Crypto World has to offer.</p>
 
 
 
