535ddeea895165171f50fa48427faff39b5fef04
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-02 17:44:56 +0300

    initial set table

--- Client/crypto-world-client/src/Components/IndexPage/Prices/Prices.js
+++ Client/crypto-world-client/src/Components/IndexPage/Prices/Prices.js
@@ -1,14 +1,38 @@
 const Prices = () => {
 
-    
-   
 
-  
+
+
+
 
 
     return (
 
-       
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                        <th>Chart</th>
+                        <th>Market Cap</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Bitcoin</td>
+                        <td>$58,752.34</td>
+                        <td></td>
+                        <td>$1,094,208,374,559</td>
+                    </tr>
+
+                </tbody>
+            </table>
+
+        </>
+
     );
 }
 
