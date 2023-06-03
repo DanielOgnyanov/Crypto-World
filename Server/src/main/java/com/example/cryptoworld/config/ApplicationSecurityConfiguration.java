e91ba2a2f87daa5e9db390f08b620ac7248013d5
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-03 18:26:25 +0300

    change sec config

--- Server/src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
+++ Server/src/main/java/com/example/cryptoworld/config/ApplicationSecurityConfiguration.java
@@ -44,6 +44,7 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
                 .antMatchers("/api/user/email/check").permitAll()
                 .antMatchers("/api/home/cryptoPrice").permitAll()
                 .antMatchers("/api/index/popular/crypto").permitAll()
+                .antMatchers("/api/crypto/marketCap24Hour").permitAll()
                 .antMatchers("/css/**","/js/**","/images/**").permitAll()
                 .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .antMatchers("/api/user/logout").permitAll()
