a87ef680b1377d79dabe8fcdf839eeb9bca5c1a1
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-03 18:19:34 +0300

    crypto controller

--- Server/src/main/java/com/example/cryptoworld/web/CryptoController.java
+++ Server/src/main/java/com/example/cryptoworld/web/CryptoController.java
@@ -1,10 +1,17 @@
 package com.example.cryptoworld.web;
 
 
+import com.example.cryptoworld.models.view.PopularCryptoViewModel;
 import com.example.cryptoworld.service.CryptoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -16,5 +23,17 @@ public class CryptoController {
 
     public CryptoController(CryptoService cryptoService) {
         this.cryptoService = cryptoService;
+    }
+
+
+
+    @GetMapping("/popular/crypto")
+    public ResponseEntity<BigDecimal> getMarketCap24Hour() {
+
+
+
+
+
+        return ;
     }
 }
