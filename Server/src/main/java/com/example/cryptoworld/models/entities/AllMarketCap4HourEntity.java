d98222b38e067ec72a120e20e3f35b28b14a9967
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-05 17:20:10 +0300

    AllMarketCap24HourEntity

--- Server/src/main/java/com/example/cryptoworld/models/entities/AllMarketCap4HourEntity.java
+++ Server/src/main/java/com/example/cryptoworld/models/entities/AllMarketCap4HourEntity.java
@@ -3,8 +3,18 @@ package com.example.cryptoworld.models.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "all_market_cap_4_hour")
-public class AllMarketCap4HourEntity {
+public class AllMarketCap4HourEntity extends BaseEntity{
+
+    private BigDecimal newMarketCap;
+    private BigDecimal oldMarketCap;
+
+
+    public AllMarketCap4HourEntity(BigDecimal newMarketCap, BigDecimal oldMarketCap) {
+        this.newMarketCap = newMarketCap;
+        this.oldMarketCap = oldMarketCap;
+    }
 }
