077a61fe581a9da6276eefedee51a36a6473ec5b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-05 17:17:48 +0300

    AllMarketCap24HourEntity

--- Server/src/main/java/com/example/cryptoworld/models/entities/AllMarketCap24HourEntity.java
+++
@@ -1,4 +0,0 @@
-package com.example.cryptoworld.models.entities;
-
-public class AllMarketCap24HourEntity {
-}
---
+++ Server/src/main/java/com/example/cryptoworld/models/entities/AllMarketCap4HourEntity.java
@@ -0,0 +1,10 @@
+package com.example.cryptoworld.models.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "all_market_cap_4_hour")
+public class AllMarketCap4HourEntity {
+}
