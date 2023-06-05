1f239fb21ae4d98dd2625873288c94c5f600ed1f
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-05 18:24:36 +0300

    MarketCap repo

--- Server/src/main/java/com/example/cryptoworld/repository/MarketCapRepository.java
+++ Server/src/main/java/com/example/cryptoworld/repository/MarketCapRepository.java
@@ -1,4 +1,9 @@
 package com.example.cryptoworld.repository;
 
-public interface MarketCapRepository {
+import com.example.cryptoworld.models.entities.AllMarketCap4HourEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MarketCapRepository extends JpaRepository<AllMarketCap4HourEntity, Long> {
 }
