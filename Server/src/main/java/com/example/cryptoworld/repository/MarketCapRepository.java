69f2008f1c531d85db61357a369100bb80a1e5f2
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-05 18:47:42 +0300

    repo ok

--- Server/src/main/java/com/example/cryptoworld/repository/MarketCapRepository.java
+++ Server/src/main/java/com/example/cryptoworld/repository/MarketCapRepository.java
@@ -2,11 +2,11 @@ package com.example.cryptoworld.repository;
 
 import com.example.cryptoworld.models.entities.AllMarketCap4HourEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
 
 @Repository
 public interface MarketCapRepository extends JpaRepository<AllMarketCap4HourEntity, Long> {
-
 }
