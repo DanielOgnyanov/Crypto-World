c83938740f29ef750e872ceb4ee442119ebde943
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-09 19:02:24 +0300

    view model crypto table

---
+++ Server/src/main/java/com/example/cryptoworld/models/view/CryptoTableViewModel.java
@@ -0,0 +1,55 @@
+package com.example.cryptoworld.models.view;
+
+import java.math.BigDecimal;
+
+public class CryptoTableViewModel {
+
+    private Long id;
+    private String name;
+    private BigDecimal volumeFor24Hour;
+    private double price;
+    private double oldPriceTrack;
+
+    public CryptoTableViewModel() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getVolumeFor24Hour() {
+        return volumeFor24Hour;
+    }
+
+    public void setVolumeFor24Hour(BigDecimal volumeFor24Hour) {
+        this.volumeFor24Hour = volumeFor24Hour;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getOldPriceTrack() {
+        return oldPriceTrack;
+    }
+
+    public void setOldPriceTrack(double oldPriceTrack) {
+        this.oldPriceTrack = oldPriceTrack;
+    }
+}
