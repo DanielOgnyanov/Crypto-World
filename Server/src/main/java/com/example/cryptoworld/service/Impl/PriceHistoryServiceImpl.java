84baacd6d02c4c90db70e2e3f3fc2627cdb527d6
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-14 20:38:18 +0300

    setPriceHistory

--- Server/src/main/java/com/example/cryptoworld/service/Impl/PriceHistoryServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/PriceHistoryServiceImpl.java
@@ -1,8 +1,19 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.repository.PriceHistoryRepository;
 import com.example.cryptoworld.service.PriceHistoryService;
 
 public class PriceHistoryServiceImpl implements PriceHistoryService {
+
+    private final PriceHistoryRepository priceHistoryRepository;
+    private final PriceHistoryService priceHistoryService;
+
+    public PriceHistoryServiceImpl(PriceHistoryRepository priceHistoryRepository, PriceHistoryService priceHistoryService) {
+        this.priceHistoryRepository = priceHistoryRepository;
+        this.priceHistoryService = priceHistoryService;
+    }
+
+
     @Override
     public void setPriceHistory() {
 
