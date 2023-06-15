9640dae27aa35954ca20707212601ab1b6a07391
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-15 20:43:59 +0300

    create sql query to group the data of history price

--- Server/src/main/java/com/example/cryptoworld/repository/PriceHistoryRepository.java
+++ Server/src/main/java/com/example/cryptoworld/repository/PriceHistoryRepository.java
@@ -2,8 +2,14 @@ package com.example.cryptoworld.repository;
 
 import com.example.cryptoworld.models.entities.PriceHistoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PriceHistoryRepository extends JpaRepository<PriceHistoryEntity, Long> {
+
+    @Query("SELECT p.name, p.price, p.recordedAt FROM PriceHistoryEntity p GROUP BY p.name")
+    List<Object[]> groupedCryptoByName();
 }
--- Server/src/main/java/com/example/cryptoworld/service/PriceHistoryService.java
+++ Server/src/main/java/com/example/cryptoworld/service/PriceHistoryService.java
@@ -1,5 +1,7 @@
 package com.example.cryptoworld.service;
 
+
+
 public interface PriceHistoryService {
 
 
