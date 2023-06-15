951e65a9bd08ce0ba65b16c42dc997f5a7348b10
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-15 20:50:20 +0300

    price history controller

--- Server/src/main/java/com/example/cryptoworld/web/PriceHistoryController.java
+++ Server/src/main/java/com/example/cryptoworld/web/PriceHistoryController.java
@@ -1,5 +1,6 @@
 package com.example.cryptoworld.web;
 
+import com.example.cryptoworld.service.PriceHistoryService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/price/history")
 public class PriceHistoryController {
+
+    private final PriceHistoryService priceHistoryService;
+
+    public PriceHistoryController(PriceHistoryService priceHistoryService) {
+        this.priceHistoryService = priceHistoryService;
+    }
 }
