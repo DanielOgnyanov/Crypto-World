e14ddfc193a2bcc2f5ccee9666ba07c811490f30
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-16 20:37:24 +0300

    price history list

--- Server/src/main/java/com/example/cryptoworld/repository/PriceHistoryRepository.java
+++ Server/src/main/java/com/example/cryptoworld/repository/PriceHistoryRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface PriceHistoryRepository extends JpaRepository<PriceHistoryEntity, Long> {
 
-    @Query("SELECT p.name, p.price, p.recordedAt FROM PriceHistoryEntity p ORDER BY p.name")
+    @Query("SELECT * FROM PriceHistoryEntity p ORDER BY p.name")
     List<Object[]> groupedCryptoByName();
 }
