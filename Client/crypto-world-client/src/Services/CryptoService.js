6a60aa794c273e90a296de58a869c170bba2f2ea
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-06-17 20:37:31 +0300

    getListOfCryptoPrices fetch data from db

--- Client/crypto-world-client/src/Services/CryptoService.js
+++ Client/crypto-world-client/src/Services/CryptoService.js
@@ -118,3 +118,28 @@ export const getPopularCryptoPrice = async () => {
       throw error;
     }
   };
+
+
+
+  export const getListOfCryptoPrices = async () => {
+    try {
+      const response = await fetch(`${baseUrl}//api/price/history/list`, {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+        },
+        
+      });
+  
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+  
+      const data = await response.json();
+  
+      return data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  };
