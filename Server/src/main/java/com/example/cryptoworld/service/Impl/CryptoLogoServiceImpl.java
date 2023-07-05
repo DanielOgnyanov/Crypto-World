721f3d825eae64c6c337765847a59e9eb2dfb59b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-07-05 17:31:53 +0300

    CryptoLogoService init

--- Server/src/main/java/com/example/cryptoworld/service/Impl/CryptoLogoServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/CryptoLogoServiceImpl.java
@@ -1,6 +1,51 @@
 package com.example.cryptoworld.service.Impl;
 
+import com.example.cryptoworld.config.InfoUtils;
+import com.example.cryptoworld.repository.CryptoRepository;
 import com.example.cryptoworld.service.CryptoLogoService;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
 
 public class CryptoLogoServiceImpl implements CryptoLogoService {
+
+    private final CryptoRepository cryptoRepository;
+    private final InfoUtils infoUtils;
+
+    public CryptoLogoServiceImpl(CryptoRepository cryptoRepository, InfoUtils infoUtils) throws IOException {
+        this.cryptoRepository = cryptoRepository;
+        this.infoUtils = infoUtils;
+    }
+
+    Response response = getResponse();
+
+
+
+
+
+
+
+
+    private Response getResponse() throws IOException {
+
+        try {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(infoUtils.getAPI_URL())
+                    .addHeader(infoUtils.getAPI_NAME(), infoUtils.getPRIVATE_KEY())
+                    .build();
+
+            return client.newCall(request).execute();
+
+
+        } catch (IOException e) {
+
+            throw new IOException(e.getMessage()
+                    + "Error occurred while executing the request to fetch crypto information");
+        }
+
+    }
 }
