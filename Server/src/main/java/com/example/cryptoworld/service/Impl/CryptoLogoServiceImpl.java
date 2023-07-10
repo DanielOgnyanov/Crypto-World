14a94450732eea078d0cdebe7fd8bb052c809729
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-07-10 21:41:20 +0300

    logo fetch

--- Server/src/main/java/com/example/cryptoworld/service/Impl/CryptoLogoServiceImpl.java
+++ Server/src/main/java/com/example/cryptoworld/service/Impl/CryptoLogoServiceImpl.java
@@ -39,6 +39,7 @@ public class CryptoLogoServiceImpl implements CryptoLogoService {
 
             JSONObject cryptoObject = jsonArray.getJSONObject(i);
 
+
             String assetId = cryptoObject.getString("asset_id");
             String iconUrl = cryptoObject.getString("url");
 
