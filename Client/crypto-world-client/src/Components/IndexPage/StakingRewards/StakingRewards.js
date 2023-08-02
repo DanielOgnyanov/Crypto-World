4a84ef85190bb92c9ef4289d5d4e06a1a7461044
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-08-02 21:24:46 +0300

    title

--- Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
+++ Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
@@ -4,13 +4,18 @@ import './StakingRewards.css'
 
 const StakingRewards = () => {
 
-  
+
 
   return (
- 
+
     <>
+      <span>
+        <strong>Earn up to 24%</strong>
+        "yearly on your crypto"
+      </span>
+
     </>
-    
+
   );
 
 }
