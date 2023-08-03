75c144a1526e1efa160ab0383e6b0cab38976760
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-08-03 19:26:49 +0300

    function registerHandler

--- Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
+++ Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
@@ -1,5 +1,6 @@
 import './StakingRewards.css'
-
+import '././Header.css'
+import { useNavigate } from "react-router-dom";
 
 
 const StakingRewards = () => {
@@ -8,27 +9,31 @@ const StakingRewards = () => {
 
   function registerHandler() {
     history('/register');
-}
+  }
 
   return (
 
-    <div id='body-content'>
+    <>
+      <div id='body-content'>
 
-      <h1 id='title'>
-        <span>
-          <strong>Earn up to 24% </strong>
-          yearly on your crypto
-        </span>
-      </h1>
+        <h1 id='title'>
+          <span>
+            <strong>Earn up to 24% </strong>
+            yearly on your crypto
+          </span>
+        </h1>
 
-      <p>
-      Staking gives you the power to earn rewards on your cash and crypto holdings. 
-      Start staking in just a couple of clicks and automatically earn rewards twice a week. 
-      Instantly unstake at any time with no penalties.
-      </p>
+        <p>
+          Staking gives you the power to earn rewards on your cash and crypto holdings.
+          Start staking in just a couple of clicks and automatically earn rewards twice a week.
+          Instantly unstake at any time with no penalties.
+        </p>
 
-    </div>
+      </div>
 
+
+      <button onClick={registerHandler} type='submit' id='navigation-button-create'>Create Account</button>
+    </>
   );
 
 }
