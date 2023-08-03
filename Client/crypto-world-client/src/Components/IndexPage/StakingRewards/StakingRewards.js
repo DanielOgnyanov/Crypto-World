e682926e0be928bf8bd9a73adb7064cdc8fc2a1b
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-08-03 19:21:39 +0300

    import css id

--- Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
+++ Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
@@ -8,14 +8,20 @@ const StakingRewards = () => {
 
   return (
 
-    <div>
-      
+    <div id='body-content'>
+
       <h1 id='title'>
         <span>
           <strong>Earn up to 24% </strong>
           yearly on your crypto
         </span>
       </h1>
+
+      <p>
+      Staking gives you the power to earn rewards on your cash and crypto holdings. 
+      Start staking in just a couple of clicks and automatically earn rewards twice a week. 
+      Instantly unstake at any time with no penalties.
+      </p>
 
     </div>
 
