501e7881bc1f05f9b7e8207975ca7df4194f1bc9
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2023-09-10 21:46:01 +0300

    p set

--- Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
+++ Client/crypto-world-client/src/Components/IndexPage/StakingRewards/StakingRewards.js
@@ -16,26 +16,25 @@ const StakingRewards = () => {
       <div id='body-content'>
 
         <h1 id='title'>
-          
+
           <span>
             <strong>Earn up to 24% </strong>
             yearly on your crypto
           </span>
         </h1>
-
         <p>
           Staking gives you the power to earn rewards on your cash and crypto holdings.
           Start staking in just a couple of clicks and automatically earn rewards twice a week.
           Instantly unstake at any time with no penalties.
         </p>
 
-      <button onClick={registerHandler} type='submit' id='stacking-rewards-button-create'>Create Account</button>
+        <button onClick={registerHandler} type='submit' id='stacking-rewards-button-create'>Create Account</button>
 
       </div>
 
     </>
-    
-    
+
+
   );
 
 }
