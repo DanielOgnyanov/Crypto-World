c5024db1ac9977b72640d7f2d9d72265c4c811b0
Daniel Ognyanov <72673385+DanielOgnyanov@users.noreply.github.com>
2024-01-15 23:29:36 +0200

    index pg change

--- Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
+++ Client/crypto-world-client/src/Components/IndexPage/IndexPage.js
@@ -39,6 +39,7 @@ const IndexPage = () => {
                 <BnbRealTimePrice />
                 <TetherRealTimePrice />
             </div>
+            
             <p id='p-hold-earn'>Hold and Earn</p>
 
             <div id='information-div'>
